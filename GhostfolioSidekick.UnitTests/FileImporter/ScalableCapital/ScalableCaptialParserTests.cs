4277588cd92df6c607052cecd8a653d184a8e7d5
Vincent Berkien <vincent.berkien@pggm.nl>
2023-08-07 15:15:35 +0200

    Added test 4 scalable capital

--- GhostfolioSidekick.UnitTests/FileImporter/ScalableCapital/ScalableCaptialParserTests.cs
+++ GhostfolioSidekick.UnitTests/FileImporter/ScalableCapital/ScalableCaptialParserTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
 using GhostfolioSidekick.FileImporter.DeGiro;
 using GhostfolioSidekick.FileImporter.ScalableCaptial;
 using GhostfolioSidekick.Ghostfolio.API;
@@ -39,6 +40,36 @@ namespace GhostfolioSidekick.UnitTests.FileImporter.ScalableCapital
 
 			// Assert
 			canParse.Should().BeTrue();
+		}
+
+		[Fact]
+		public async Task ConvertToOrders_Example1()
+		{
+			// Arrange
+			var parser = new ScalableCapitalParser(api.Object);
+			var fixture = new Fixture();
+
+			var asset = fixture.Build<Asset>().With(x => x.Currency, "EUR").Create();
+			var account = fixture.Create<Account>();
+
+			api.Setup(x => x.GetAccountByName(account.Name)).ReturnsAsync(account);
+			api.Setup(x => x.FindSymbolByISIN("IE00077FRP95")).ReturnsAsync(asset);
+
+			// Act
+			var orders = await parser.ConvertToOrders(account.Name, "./FileImporter/ScalableCapital/WUMExample1.csv");
+
+			// Assert
+			orders.Should().BeEquivalentTo(new[] { new Order {
+				AccountId = account.Id,
+				Asset = asset,
+				Comment = "Transaction Reference: [SCALQbWiZnN9DtQ]",
+				Currency = asset.Currency,
+				Date = new DateTime(2023,8,3, 0,0,0, DateTimeKind.Utc),
+				Fee = 0.99M,
+				Quantity = 5,
+				Type = OrderType.BUY,
+				UnitPrice = 8.685M
+			} });
 		}
 	}
 }
