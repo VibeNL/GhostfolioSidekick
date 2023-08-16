5590de9bf2de1f552b5080d8e2f1218b21246922
VibeNL <vpberkie@gmail.com>
2023-08-16 10:02:06 +0200

    Adding some logging when no importer can be found

---
+++ GhostfolioSidekick/FileImporter/NoImporterAvailableException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace GhostfolioSidekick.FileImporter
+{
+    [Serializable]
+    internal class NoImporterAvailableException : Exception
+    {
+        public NoImporterAvailableException()
+        {
+        }
+
+        public NoImporterAvailableException(string? message) : base(message)
+        {
+        }
+
+        public NoImporterAvailableException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoImporterAvailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
