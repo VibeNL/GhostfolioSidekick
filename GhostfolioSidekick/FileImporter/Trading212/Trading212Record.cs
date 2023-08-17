5c1e635327d7b5f4456c9e0859ca00ee0cec6f5b
VibeNL <vpberkie@gmail.com>
2023-08-17 13:34:20 +0200

    Adding dividend 4 trading 212 (#19)

--- GhostfolioSidekick/FileImporter/Trading212/Trading212Record.cs
+++ GhostfolioSidekick/FileImporter/Trading212/Trading212Record.cs
@@ -1,51 +1,71 @@
-﻿using CsvHelper.Configuration.Attributes;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
 
 namespace GhostfolioSidekick.FileImporter.Trading212
 {
-    public class Trading212Record
-    {
-        public string Action { get; set; }
- 
-        public DateTime Time { get; set; }
+	public class Trading212Record
+	{
+		public string Action { get; set; }
 
-        public string ISIN { get; set; }
+		public DateTime Time { get; set; }
 
-        public string Ticker { get; set; }
+		public string ISIN { get; set; }
 
-        public string Name { get; set; }
+		public string Ticker { get; set; }
 
-        [Name("No. of shares")]
-        public decimal? NumberOfShares { get; set; }
+		public string Name { get; set; }
 
-        [Name("Price / share")]
-        public decimal? Price { get; set; }
+		[Name("No. of shares")]
+		public decimal? NumberOfShares { get; set; }
 
-        [Name("Currency (Price / share)")]
-        public string Currency { get; set; }
+		[Name("Price / share")]
+		public decimal? Price { get; set; }
 
-        [Name("Exchange rate")]
-        public decimal? ExchangeRate { get; set; }
+		[Name("Currency (Price / share)")]
+		public string Currency { get; set; }
 
-        [Name("Currency (Result)")]
-        public string CurrencySource { get; set; }
+		[Name("Exchange rate")]
+		[TypeConverter(typeof(ExchangeRateConverter))]
+		public decimal? ExchangeRate { get; set; }
 
-        [Optional]
-        [Name("Stamp duty reserve tax")]
-        public decimal? FeeUK { get; set; }
+		[Name("Currency (Result)")]
+		public string CurrencySource { get; set; }
 
-        [Optional]
-        [Name("Currency (Stamp duty reserve tax)")]
-        public string FeeUKCurrency { get; set; }
+		[Optional]
+		[Name("Stamp duty reserve tax")]
+		public decimal? FeeUK { get; set; }
 
-        public string Notes { get; set; }
+		[Optional]
+		[Name("Currency (Stamp duty reserve tax)")]
+		public string FeeUKCurrency { get; set; }
 
-        [Name("ID")]
-        public string Id { get; set; }
+		public string Notes { get; set; }
 
-        [Name("Currency conversion fee")]
-        public decimal? ConversionFee { get; set; }
+		[Name("ID")]
+		public string Id { get; set; }
 
-        [Name("Currency (Currency conversion fee)")]
-        public string ConversionFeeCurrency { get; set; }
-    }
+		[Name("Currency conversion fee")]
+		public decimal? ConversionFee { get; set; }
+
+		[Name("Currency (Currency conversion fee)")]
+		public string ConversionFeeCurrency { get; set; }
+	}
+
+	internal class ExchangeRateConverter : DefaultTypeConverter
+	{
+		public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+		{
+			switch (text)
+			{
+				case "":
+					return null;
+				case "Not available":
+					return null;
+			}
+
+			return decimal.Parse(text);
+		}
+	}
 }
