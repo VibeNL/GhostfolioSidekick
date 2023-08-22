a7af1478913c993cf8479f42c762818db4504e30
Vincent Berkien <vincent.berkien@pggm.nl>
2023-08-22 09:10:23 +0200

    Added test file

---
+++ GhostfolioSidekick/FileImporter/Coinbase/CoinbaseParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostfolioSidekick.FileImporter.Coinbase
+{
+	public class CoinbaseParser
+	{
+	}
+}
---
+++ GhostfolioSidekick/FileImporter/Coinbase/CoinbaseRecord.cs
@@ -0,0 +1,13 @@
+﻿using GhostfolioSidekick.FileImporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostfolioSidekick.FileImporter.Coinbase
+{
+	public class CoinbaseRecord
+	{
+	}
+}
