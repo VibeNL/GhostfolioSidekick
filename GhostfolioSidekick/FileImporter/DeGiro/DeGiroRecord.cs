365d34d2f84bf02388ce62a22f76b4f3d3f4f03c
VibeNL <vpberkie@gmail.com>
2023-09-19 15:09:56 +0200

    Fix date error

--- GhostfolioSidekick/FileImporter/DeGiro/DeGiroRecord.cs
+++ GhostfolioSidekick/FileImporter/DeGiro/DeGiroRecord.cs
@@ -3,35 +3,36 @@ using System.Globalization;
 
 namespace GhostfolioSidekick.FileImporter.DeGiro
 {
-    [Delimiter(",")]
-    public class DeGiroRecord
-    {
-        [Format("dd-MM-yyyy")]
-        public DateOnly Datum { get; set; }
+	[Delimiter(",")]
+	public class DeGiroRecord
+	{
+		[Format("dd-MM-yyyy")]
+		public DateOnly Datum { get; set; }
 
-        public TimeOnly Tijd { get; set; }
+		public TimeOnly Tijd { get; set; }
 
-        public DateOnly Valutadatum { get; set; }
+		[Format("dd-MM-yyyy")]
+		public DateOnly Valutadatum { get; set; }
 
-        public string Product { get; set; }
+		public string Product { get; set; }
 
-        public string ISIN { get; set; }
+		public string ISIN { get; set; }
 
-        public string Omschrijving { get; set; }
+		public string Omschrijving { get; set; }
 
-        public string FX { get; set; }
+		public string FX { get; set; }
 
-        public string Mutatie { get; set; }
+		public string Mutatie { get; set; }
 
-        [Index(8)]
-        [CultureInfo("nl-NL")]
-        public decimal? Total { get; set; }
+		[Index(8)]
+		[CultureInfo("nl-NL")]
+		public decimal? Total { get; set; }
 
-        public string Saldo { get;set; }
+		public string Saldo { get; set; }
 
-        [Name("Order Id")]
-        public string OrderId { get; set; }
+		[Name("Order Id")]
+		public string OrderId { get; set; }
 
 
-    }
+	}
 }
