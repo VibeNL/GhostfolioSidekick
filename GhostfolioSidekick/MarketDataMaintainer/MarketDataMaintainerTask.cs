d9453af45b3cd8f0d037612e407bf78199b25195
Vincent Berkien <vincent.berkien@pggm.nl>
2023-10-10 08:39:41 +0200

    added some logging

--- GhostfolioSidekick/MarketDataMaintainer/MarketDataMaintainerTask.cs
+++ GhostfolioSidekick/MarketDataMaintainer/MarketDataMaintainerTask.cs
@@ -1,10 +1,28 @@
-﻿namespace GhostfolioSidekick.MarketDataMaintainer
+﻿using GhostfolioSidekick.FileImporter;
+using GhostfolioSidekick.Ghostfolio.API;
+using Microsoft.Extensions.Logging;
+
+namespace GhostfolioSidekick.MarketDataMaintainer
 {
 	internal class MarketDataMaintainerTask : IScheduledWork
 	{
-		public Task DoWork()
+		private readonly ILogger<FileImporterTask> logger;
+		private readonly IGhostfolioAPI api;
+
+		public MarketDataMaintainerTask(
+			ILogger<FileImporterTask> logger,
+			IGhostfolioAPI api)
 		{
-			throw new NotImplementedException();
+			this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			this.api = api ?? throw new ArgumentNullException(nameof(api));
+		}
+
+		public async Task DoWork()
+		{
+			logger.LogInformation($"{nameof(MarketDataMaintainerTask)} Starting to do work");
+
+
+			logger.LogInformation($"{nameof(MarketDataMaintainerTask)} Done");
 		}
 	}
 }
