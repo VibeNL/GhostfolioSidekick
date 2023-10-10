ac1ac601b793a979dbf0e0b71f326586d47e8f3c
Vincent Berkien <vincent.berkien@pggm.nl>
2023-10-10 08:37:17 +0200

    created skeleton

---
+++ GhostfolioSidekick/MarketDataMaintainer/MarketDataMaintainerTask.cs
@@ -0,0 +1,10 @@
+﻿namespace GhostfolioSidekick.MarketDataMaintainer
+{
+	internal class MarketDataMaintainerTask : IScheduledWork
+	{
+		public Task DoWork()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
