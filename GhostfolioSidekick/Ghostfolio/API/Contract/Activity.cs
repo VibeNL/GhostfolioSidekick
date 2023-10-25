de022ce44156ebb5c013871095e8470f6b8d4cc8
Vincent Berkien <vincent.berkien@pggm.nl>
2023-10-25 13:07:39 +0200

    Remove unused code

--- GhostfolioSidekick/Ghostfolio/API/Contract/Activity.cs
+++ GhostfolioSidekick/Ghostfolio/API/Contract/Activity.cs
@@ -22,70 +22,7 @@
 
 		public decimal UnitPrice { get; set; }
 
-
 		// Internal use
 		public string ReferenceCode { get; set; }
-
-		public Activity Merge(Activity activity)
-		{
-			if (activity == null) throw new ArgumentNullException();
-			if (Type == ActivityType.IGNORE)
-			{
-				return this;
-			}
-
-			var canJoin =
-				(Type == ActivityType.BUY || Type == ActivityType.SELL) &&
-				(activity.Type == ActivityType.BUY || activity.Type == ActivityType.SELL) &&
-				AccountId == activity.AccountId &&
-				Asset.Symbol == activity.Asset.Symbol &&
-				Currency == activity.Currency &&
-				FeeCurrency == activity.FeeCurrency;
-
-			if (!canJoin)
-			{
-				throw new NotSupportedException();
-			}
-
-			var positiveOrNegativeThis = Type == ActivityType.BUY ? 1 : -1;
-			var positiveOrNegativeOther = activity.Type == ActivityType.BUY ? 1 : -1;
-
-			decimal totalQuantity = Quantity + activity.Quantity;
-			var unitPrice = totalQuantity == 0 ? 0 :
-					((UnitPrice * Quantity) + (activity.UnitPrice * activity.Quantity))
-					/
-					totalQuantity;
-
-			decimal quantity = positiveOrNegativeThis * Quantity + positiveOrNegativeOther * activity.Quantity;
-
-			ActivityType activityType;
-			if (quantity == 0)
-			{
-				activityType = ActivityType.IGNORE;
-			}
-			else if (quantity > 0)
-			{
-				activityType = ActivityType.BUY;
-			}
-			else
-			{
-				activityType = ActivityType.SELL;
-			}
-
-			return new Activity
-			{
-				Type = activityType,
-				AccountId = AccountId,
-				Asset = Asset,
-				Comment = Comment,
-				Currency = Currency,
-				FeeCurrency = FeeCurrency,
-				Date = Date,
-				Fee = Fee + activity.Fee,
-				Quantity = Math.Abs(quantity),
-				ReferenceCode = ReferenceCode,
-				UnitPrice = Math.Abs(unitPrice)
-			};
-		}
 	}
 }
