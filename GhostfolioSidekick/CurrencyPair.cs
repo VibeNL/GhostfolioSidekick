94c8c3ccbdf65d5421afde54a21cb26bcbfefe20
VibeNL <vpberkie@gmail.com>
2023-11-16 12:34:12 +0100

    Enable known currency conversions (GBP -> GBp) when one is not available. (#53)

---
+++ GhostfolioSidekick/CurrencyPair.cs
@@ -0,0 +1,49 @@
+﻿using GhostfolioSidekick.Model;
+using System.Collections;
+
+namespace GhostfolioSidekick
+{
+	internal class CurrencyPair : IEnumerable<Currency>
+	{
+		public CurrencyPair(Currency a, Currency b, decimal conversionRateAToB)
+		{
+			A = a;
+			B = b;
+			ConversionRateAToB = conversionRateAToB;
+		}
+
+		public Currency A { get; }
+		public Currency B { get; }
+		public decimal ConversionRateAToB { get; }
+
+		public decimal CalculateRate(Currency currency, bool inversion)
+		{
+			if (currency == A)
+			{
+				return 1;
+			}
+
+			return inversion ? 1 / ConversionRateAToB : ConversionRateAToB;
+		}
+
+		public IEnumerator<Currency> GetEnumerator()
+		{
+			return GetList();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetList();
+		}
+
+		private IEnumerator<Currency> GetList()
+		{
+			if (A == B)
+			{
+				return new[] { A }.ToList().GetEnumerator();
+			}
+
+			return new[] { A, B }.ToList().GetEnumerator();
+		}
+	}
+}
