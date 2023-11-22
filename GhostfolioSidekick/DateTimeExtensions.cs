00e1888c69d16be317c9815efa901bb104cdf688
VibeNL <vpberkie@gmail.com>
2023-11-22 08:03:17 +0100

    Setting date and time to a fixed culture (#52)
    Assuming PR solves the issue due to no response.

---
+++ GhostfolioSidekick/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace GhostfolioSidekick
+{
+	internal static class DateTimeExtensions
+	{
+		internal static string ToInvariantDateOnlyString(this DateTime dateTime)
+		{
+			return dateTime.ToString("yyyy-MM-dd");
+		}
+
+		internal static string ToInvariantString(this DateOnly dateOnly)
+		{
+			return dateOnly.ToString("yyyy-MM-dd");
+		}
+
+		internal static string ToInvariantString(this TimeOnly timeOnly)
+		{
+			return timeOnly.ToString("HH:mm");
+		}
+	}
+}
