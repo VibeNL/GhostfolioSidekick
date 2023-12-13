50f8005b845c4ebb4b69e342a5627006300760cc
VibeNL <vpberkie@gmail.com>
2023-12-13 14:46:46 +0100

    Improve crypto matching (#72)
    * - 'Borrowed' the crypto list from ghostfolio
    - use a fixed list of crypto to convert coins
    
    * fixing tests

---
+++ GhostfolioSidekick/FileImporter/Crypto/CryptoMapper.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace GhostfolioSidekick.FileImporter.Crypto
+{
+	internal class CryptoMapper
+	{
+		private Dictionary<string, string> mappings = new Dictionary<string, string>();
+
+		private CryptoMapper()
+		{
+			var assembly = typeof(CryptoMapper).Assembly;
+			using var resource = assembly.GetManifestResourceStream("GhostfolioSidekick.FileImporter.Crypto.cryptocurrencies.json");
+			using var streamReader = new StreamReader(resource);
+			var fileContent = streamReader.ReadToEnd();
+			var obj = JObject.Parse(fileContent);
+			foreach (var item in obj)
+			{
+				mappings.Add(item.Key, item.Value.ToString());
+			}
+		}
+
+		public static readonly CryptoMapper Instance = new CryptoMapper();
+
+		internal string? GetFullname(string symbol)
+		{
+			return mappings.TryGetValue(symbol, out var value) ? value : symbol;
+		}
+	}
+}
