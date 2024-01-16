98dc04086ea017a47bedb8734c308b1bf4a19319
Vincent Berkien <vincent.berkien@pggm.nl>
2024-01-16 12:57:24 +0100

    update model

--- Model/Activities/Activity.cs
+++ Model/Activities/Activity.cs
@@ -4,9 +4,23 @@ namespace GhostfolioSidekick.Model.Activities
 {
 	public class Activity(
 		ActivityType activityType,
-		SymbolProfile? asset)
+		SymbolProfile? asset,
+		DateTime dateTime,
+		decimal quantity,
+		Money unitPrice)
 	{
 		public ActivityType ActivityType { get; } = activityType;
+
 		public SymbolProfile? Asset { get; } = asset;
+
+		public DateTime Date { get; set; } = dateTime;
+
+		public IEnumerable<Money> Fees { get; set; } = [];
+
+		public decimal Quantity { get; set; } = quantity;
+
+		public Money UnitPrice { get; set; } = unitPrice;
+
+		public Money? Taxes { get; set; }
 	}
 }
---
+++ Model/Money.cs
@@ -0,0 +1,9 @@
+﻿namespace GhostfolioSidekick.Model
+{
+	public class Money(decimal amount, Currency currency)
+	{
+		public decimal Amount { get; set; } = amount;
+
+		public Currency Currency { get; set; } = currency;
+	}
+}
