9a31c10c00294ef920faaae38185c0a348cd01af
VibeNL <vpberkie@gmail.com>
2024-01-31 14:43:31 +0100

    sanity check main

---
+++ GhostfolioSidekick.UnitTests/ProgramTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GhostfolioSidekick.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using RestSharp;
+
+namespace GhostfolioSidekick.UnitTests
+{
+	public class ProgramTests
+	{
+		[Fact]
+		public void CheckIfEverythingIsRegistered()
+		{
+			// Arrange
+			var st = new Mock<IApplicationSettings>();
+			st.Setup(x => x.GhostfolioUrl).Returns("https://dummy");
+			st.Setup(x => x.ConfigurationInstance).Returns(new ConfigurationInstance());
+			var rc = new Mock<IRestClient>();
+
+			var testHost = Program
+			.CreateHostBuilder()
+			.ConfigureServices((hostContext, services) =>
+			{
+				services.AddSingleton(st.Object);
+				services.AddSingleton(rc.Object);
+			})
+				.Build();
+
+			// Act
+			var host = testHost.Services.GetService<IHostedService>();
+
+			//
+			host.Should().NotBeNull();
+		}
+	}
+}
