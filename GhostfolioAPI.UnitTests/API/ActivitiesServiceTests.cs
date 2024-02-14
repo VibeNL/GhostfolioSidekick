fb1188e5d97776c74f1a8600f3f29f214031bf9c
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-02-14 11:08:42 +0100

    Some tests

--- GhostfolioAPI.UnitTests/API/ActivitiesServiceTests.cs
+++ GhostfolioAPI.UnitTests/API/ActivitiesServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using AutoFixture;
 using FluentAssertions;
 using GhostfolioSidekick.GhostfolioAPI.API;
+using GhostfolioSidekick.GhostfolioAPI.API.Mapper;
 using GhostfolioSidekick.Model.Accounts;
+using GhostfolioSidekick.Model.Activities;
 using GhostfolioSidekick.Model.Compare;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -14,23 +16,23 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API
 	{
 		private const string orderUrl = "api/v1/order";
 
-		private readonly Fixture fixture = new Fixture();
+
+		private readonly Fixture fixture = DefaultFixture.Create();
+		private readonly Mock<IExchangeRateService> exchangeRateService;
 		private readonly Mock<IAccountService> accountService;
 		private readonly ActivitiesService activitiesService;
 
 		public ActivitiesServiceTests()
 		{
 			var loggerMock = new Mock<ILogger<ActivitiesService>>();
-			var exchange = new Mock<IExchangeRateService>();
+			exchangeRateService = new Mock<IExchangeRateService>();
 			accountService = new Mock<IAccountService>();
 
 			activitiesService = new ActivitiesService(
-				exchange.Object,
+				exchangeRateService.Object,
 				accountService.Object,
 				restCall: restCall,
 				logger: loggerMock.Object);
-
-			fixture.Customize(new ContractConventions());
 		}
 
 		[Fact]
@@ -60,6 +62,75 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API
 				x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl)), default),
 				Times.Once);
 			activities.Should().HaveCount(10);
+		}
+
+		[Fact]
+		public async Task InsertActivity_Success()
+		{
+			// Arrange
+			var symbolProfile = fixture.Create<Model.Symbols.SymbolProfile>();
+			var newActivity = DefaultFixture.Create().Create<Activity>();
+
+			restClient
+				.Setup(x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Post), default))
+				.ReturnsAsync(CreateResponse(true, string.Empty));
+
+			// Act
+			await activitiesService.InsertActivity(symbolProfile, newActivity);
+
+			// Assert
+			restClient.Verify(
+				x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Post), default),
+				Times.Once);
+		}
+
+		[Fact]
+		public async Task UpdateActivity_Success()
+		{
+			// Arrange
+			var symbolProfile = fixture.Create<Model.Symbols.SymbolProfile>();
+			var oldActivity = DefaultFixture.Create().Create<Activity>();
+			var newActivity = DefaultFixture.Create().Create<Activity>();
+
+			restClient
+				.Setup(x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Delete), default))
+				.ReturnsAsync(CreateResponse(true, string.Empty));
+
+			restClient
+				.Setup(x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Post), default))
+				.ReturnsAsync(CreateResponse(true, string.Empty));
+
+			// Act
+			await activitiesService.UpdateActivity(symbolProfile, oldActivity, newActivity);
+
+			// Assert
+			restClient.Verify(
+				x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Delete), default),
+				Times.Once);
+
+			restClient.Verify(
+				x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Post), default),
+				Times.Once);
+		}
+
+		[Fact]
+		public async Task DeleteActivity_Success()
+		{
+			// Arrange
+			var symbolProfile = fixture.Create<Model.Symbols.SymbolProfile>();
+			var oldActivity = DefaultFixture.Create().Create<Activity>();
+
+			restClient
+				.Setup(x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Delete), default))
+				.ReturnsAsync(CreateResponse(true, string.Empty));
+
+			// Act
+			await activitiesService.DeleteActivity(symbolProfile, oldActivity);
+
+			// Assert
+			restClient.Verify(
+				x => x.ExecuteAsync(It.Is<RestRequest>(x => x.Resource.Contains(orderUrl) && x.Method == Method.Delete), default),
+				Times.Once);
 		}
 	}
 }
---
+++ GhostfolioAPI.UnitTests/DefaultFixture.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using GhostfolioSidekick.Model.Activities;
+using System.Reflection;
+
+namespace GhostfolioSidekick.GhostfolioAPI.UnitTests
+{
+	public class DefaultFixture
+	{
+		public static Fixture Create()
+		{
+			var fixture = new Fixture();
+			fixture.Customize<Contract.Activity>(composer =>
+			composer
+				.With(p => p.Type, Contract.ActivityType.BUY));
+			fixture.Customize<Contract.SymbolProfile>(composer =>
+			composer
+				.With(p => p.AssetClass, AssetClass.Equity.ToString().ToUpperInvariant())
+				.With(p => p.AssetSubClass, AssetSubClass.Etf.ToString().ToUpperInvariant()));
+			fixture.Customizations.Add(new ActivityBuilder());
+			return fixture;
+		}
+	}
+
+	public class ActivityBuilder : ISpecimenBuilder
+	{
+		public ActivityBuilder()
+		{
+		}
+
+		public object Create(object request, ISpecimenContext context)
+		{
+			var pi = request as ParameterInfo;
+			if (pi == null)
+			{
+				return new NoSpecimen();
+			}
+
+			if (pi.Member.DeclaringType == typeof(Activity) &&
+				pi.ParameterType == typeof(ActivityType))
+			{
+				return ActivityType.Buy;
+			}
+
+			return new NoSpecimen();
+		}
+	}
+}
