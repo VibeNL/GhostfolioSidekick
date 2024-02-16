e0f84061089823258b5c7c8ed0cb55ce7402efb6
VibeNL <30174292+VibeNL@users.noreply.github.com>
2024-02-16 20:37:10 +0100

    implement determine prices for crypto when is 0

--- GhostfolioAPI/Strategies/DeterminePrice.cs
+++ GhostfolioAPI/Strategies/DeterminePrice.cs
@@ -4,29 +4,29 @@ using GhostfolioSidekick.Model.Activities;
 
 namespace GhostfolioSidekick.GhostfolioAPI.Strategies
 {
-	public class DeterminePrice(/*IMarketDataService marketDataService*/) : IHoldingStrategy
+	public class DeterminePrice(IMarketDataService marketDataService) : IHoldingStrategy
 	{
 		public int Priority => (int)CryptoStrategiesPriority.DeterminePrice;
 
 		public Task Execute(Holding holding)
 		{
 			//// TODO
-			////var activities = holding.Activities.OrderBy(x => x.Date).ToList();
+			var activities = holding.Activities.OrderBy(x => x.Date).ToList();
 
-			////foreach (var activity in activities.Where(x => x.UnitPrice.Amount == 0))
-			////{
-			////	activity.UnitPrice = await GetUnitPrice(holding.SymbolProfile, activity.Date);
-			////}
+			foreach (var activity in activities.Where(x => x.UnitPrice.Amount == 0))
+			{
+				activity.UnitPrice = await GetUnitPrice(holding.SymbolProfile, activity.Date);
+			}
 
 			return Task.CompletedTask;
 		}
-		/*
+		
 		private async Task<Money> GetUnitPrice(SymbolProfile? symbolProfile, DateTime date)
 		{
 			var md = (await marketDataService.GetAllSymbolProfiles()).ToList();
 			var profile = md.Single(x => x.Equals(symbolProfile));
 			var marketDate = profile.MarketData.Single(x => x.Date == date);
 			return new Money(symbolProfile!.Currency, marketDate.MarketPrice);
-		}*/
+		}
 	}
 }
