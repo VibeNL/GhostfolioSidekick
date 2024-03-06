c93e36448f560da7a5fbf41a70077b927b2c832b
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-03-06 14:35:27 +0100

    some more tests

--- GhostfolioAPI.UnitTests/API/Mapper/ContractToModelMapperTests.cs
+++ GhostfolioAPI.UnitTests/API/Mapper/ContractToModelMapperTests.cs
@@ -1,7 +1,9 @@
 using AutoFixture;
 using FluentAssertions;
 using GhostfolioSidekick.GhostfolioAPI.API.Mapper;
+using GhostfolioSidekick.GhostfolioAPI.Contract;
 using GhostfolioSidekick.Model.Activities;
+using System.Linq;
 
 namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API
 {
@@ -143,11 +145,20 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API
 		public void MapToHoldings_ShouldReturnCorrectHoldings()
 		{
 			// Arrange
+			int c = -1;
+			ActivityType GetRandomType()
+			{
+				var values = Enum.GetValues(typeof(ActivityType)).OfType<ActivityType>().Where(x => x != ActivityType.IGNORE).ToList();
+				c = (c + 1) % values.Count;
+				return values[c];
+			}
+
 			var accounts = new Fixture().CreateMany<Model.Accounts.Account>(1).ToArray();
 			var activities = new Fixture().Customize(new AssetCustomization())
-				.Build<Contract.Activity>()
+				.Build<Activity>()
 				.With(x => x.AccountId, accounts[0].Id)
-				.CreateMany(1)
+				.With(x => x.Type, () => GetRandomType())
+				.CreateMany(100)
 				.ToArray();
 
 			// Act
@@ -163,15 +174,60 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API
 			result[0].SymbolProfile.AssetClass.Should().Be(Utilities.ParseAssetClass(activities[0].SymbolProfile.AssetClass));
 			result[0].SymbolProfile.AssetSubClass.Should().Be(Utilities.ParseAssetSubClass(activities[0].SymbolProfile.AssetSubClass));
 		}
+
+		[Fact]
+		public void MapToHoldings_InvalidType_ShouldReturnCorrectHoldings()
+		{
+			// Arrange
+			var accounts = new Fixture().CreateMany<Model.Accounts.Account>(1).ToArray();
+			var activities = new Fixture().Customize(new AssetCustomization())
+				.Build<Activity>()
+				.With(x => x.AccountId, accounts[0].Id)
+				.With(x => x.Type, (ActivityType)42)
+				.CreateMany(100)
+				.ToArray();
+
+			// Act
+			var result = () => ContractToModelMapper.MapToHoldings(accounts, activities).ToList();
+
+			// Assert
+			result.Should().Throw<NotSupportedException>().WithMessage("ActivityType 42 not supported");
+		}
+
+		[Fact]
+		public void MapToHoldings_GeneratedSymbol_ShouldReturnCorrectHoldings()
+		{
+			// Arrange
+			var accounts = new Fixture().CreateMany<Model.Accounts.Account>(1).ToArray();
+			var activities = new Fixture().Customize(new AssetCustomization())
+				.Build<Activity>()
+				.With(x => x.AccountId, accounts[0].Id)
+				.With(x => x.Type, ActivityType.BUY)
+				.With(x => x.SymbolProfile, new SymbolProfile {
+					AssetClass = null,
+					AssetSubClass = null,
+					DataSource = "MANUAL",
+					Symbol = Guid.NewGuid().ToString(),
+					Currency = "USD",
+					Name = "Dummy",
+					Countries = [],
+					Sectors = []
+				})
+				.CreateMany(1)
+				.ToArray();
+
+			// Act
+			var result = ContractToModelMapper.MapToHoldings(accounts, activities).ToList();
+
+			// Assert
+			result[0].SymbolProfile.Should().BeNull();
+		}
 	}
 
 	internal class AssetCustomization : ICustomization
 	{
 		public void Customize(IFixture fixture)
 		{
-			fixture.Customize<Contract.Activity>(composer =>
-			composer
-				.With(p => p.Type, Contract.ActivityType.BUY));
 			fixture.Customize<Contract.SymbolProfile>(composer =>
 			composer
 				.With(p => p.AssetClass, AssetClass.Equity.ToString().ToUpperInvariant())
