0d3f87af68b9f4ea450059bf112c9cc9eff559c7
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-03-07 09:28:21 +0100

    some extra tests

--- GhostfolioAPI.UnitTests/API/Mapper/ModelToContractMapperTests.cs
+++ GhostfolioAPI.UnitTests/API/Mapper/ModelToContractMapperTests.cs
@@ -190,6 +190,25 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API.Mapper
 		}
 
 		[Fact]
+		public async Task ConvertToGhostfolioActivity_GiftActivityWithoutSymbol_Success()
+		{
+			// Arrange
+			var activity = DefaultFixture.Create().Create<GiftActivity>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(exchangeRateServiceMock.Object, null, activity);
+
+			// Assert
+			result.Should().NotBeNull();
+			result.Type.Should().Be(Contract.ActivityType.BUY);
+			result.SymbolProfile!.Symbol.Should().Be(activity.Description);
+			result.SymbolProfile.DataSource.Should().Be(ManualDataSource);
+			result.Date.Should().Be(activity.Date);
+			result.Quantity.Should().Be(activity.Amount);
+			// TODO check more
+		}
+
+		[Fact]
 		public async Task ConvertToGhostfolioActivity_SendAndReceiveActivity_Success()
 		{
 			// Arrange
@@ -206,6 +225,28 @@ namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API.Mapper
 			result.SymbolProfile.DataSource.Should().Be(symbolProfile.DataSource);
 			result.Date.Should().Be(activity.Date);
 			result.Quantity.Should().Be(activity.Quantity);
+			result.UnitPrice.Should().Be(activity.UnitPrice!.Amount);
+			// TODO check more
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_SendAndReceiveActivity_Send_Success()
+		{
+			// Arrange
+			var symbolProfile = DefaultFixture.Create().Create<Model.Symbols.SymbolProfile>();
+			var activity = DefaultFixture.Create().Create<SendAndReceiveActivity>();
+			activity.Quantity = -activity.Quantity;
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(exchangeRateServiceMock.Object, symbolProfile, activity);
+
+			// Assert
+			result.Should().NotBeNull();
+			result.Type.Should().Be(Contract.ActivityType.SELL);
+			result.SymbolProfile!.Symbol.Should().Be(symbolProfile.Symbol);
+			result.SymbolProfile.DataSource.Should().Be(symbolProfile.DataSource);
+			result.Date.Should().Be(activity.Date);
+			result.Quantity.Should().Be(-activity.Quantity);
 			result.UnitPrice.Should().Be(activity.UnitPrice!.Amount);
 			// TODO check more
 		}
