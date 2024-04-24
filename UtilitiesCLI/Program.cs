f6e166f70e86abf66583973e0bdc3c5ff4178e1b
VibeNL <30174292+VibeNL@users.noreply.github.com>
2024-04-24 08:43:56 +0200

    Move to another pdf lib, part 1

--- UtilitiesCLI/Program.cs
+++ UtilitiesCLI/Program.cs
@@ -1,47 +1,93 @@
-﻿using UglyToad.PdfPig;
-using UglyToad.PdfPig.Content;
-using UglyToad.PdfPig.Writer;
-using System.Linq;
-using UglyToad.PdfPig.Fonts.Standard14Fonts;
+﻿using PdfSharp.Pdf.Content;
+using PdfSharp.Pdf.Content.Objects;
+using PdfSharp.Pdf.IO;
 
 namespace UtilitiesCLI
 {
 	internal class Program
 	{
+		private static List<string> forbiddenwords;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Anonymize PDF");
 
 			var sourceFile = args[0];
 			var targetFile = args[1];
-			var forbiddenwords = args.Skip(2).ToList();
+			forbiddenwords = args.Skip(2).ToList();
 
-			using (PdfDocument document = PdfDocument.Open(sourceFile))
+			File.Copy(sourceFile, targetFile, true);
+			using (var document = PdfReader.Open(targetFile, PdfDocumentOpenMode.Modify))
 			{
-				PdfDocumentBuilder builder = new PdfDocumentBuilder();
-				// Fonts must be registered with the document builder prior to use to prevent duplication.
-				PdfDocumentBuilder.AddedFont font = builder.AddStandard14Font(Standard14Font.TimesRoman);
-
-				for (var i = 0; i < document.NumberOfPages; i++)
+				foreach (var page in document.Pages)
 				{
-					var sourcePage = document.GetPage(i + 1);
-					var targetPage = builder.AddPage(sourcePage.Width, sourcePage.Height);
-					foreach (var word in from word in sourcePage.GetWords()
-										 select word)
+					var contents = ContentReader.ReadContent(page);
+					foreach (var item in contents)
 					{
-						targetPage.AddText(
-							ReplaceText(forbiddenwords, word.Text),
-							(decimal)word.Letters[0].FontSize,
-							new UglyToad.PdfPig.Core.PdfPoint(
-								word.BoundingBox.Left, word.BoundingBox.Bottom),
-								font);
+						ReplaceText(item);
 					}
 				}
 
-				byte[] documentBytes = builder.Build();
-				File.WriteAllBytes(targetFile, documentBytes);
+				// Save PDF with new name.
+				document.Save(targetFile);
 			}
+		}
 
+		private static void ReplaceText(CObject obj)
+		{
+			if (obj is CArray)
+				ReplaceText((CArray)obj);
+			else if (obj is CComment)
+				ReplaceText((CComment)obj);
+			else if (obj is CInteger)
+				ReplaceText((CInteger)obj);
+			else if (obj is CName)
+				ReplaceText((CName)obj);
+			else if (obj is CNumber)
+				ReplaceText((CNumber)obj);
+			else if (obj is COperator)
+				ReplaceText((COperator)obj);
+			else if (obj is CReal)
+				ReplaceText((CReal)obj);
+			else if (obj is CSequence)
+				ReplaceText((CSequence)obj);
+			else if (obj is CString)
+				ReplaceText((CString)obj);
+			else
+				throw new NotImplementedException(obj.GetType().AssemblyQualifiedName);
+		}
+		private static void ReplaceText(CArray obj)
+		{
+			foreach (var element in obj)
+			{
+				ReplaceText(element);
+			}
+		}
+		private static void ReplaceText(CComment obj) { /* nothing */ }
+		private static void ReplaceText(CInteger obj) { /* nothing */ }
+		private static void ReplaceText(CName obj) { /* nothing */ }
+		private static void ReplaceText(CNumber obj) { /* nothing */ }
+		private static void ReplaceText(COperator obj)
+		{
+			if (obj.OpCode.OpCodeName == OpCodeName.Tj || obj.OpCode.OpCodeName == OpCodeName.TJ)
+			{
+				foreach (var element in obj.Operands)
+				{
+					ReplaceText(element);
+				}
+			}
+		}
+		private static void ReplaceText(CReal obj) { /* nothing */ }
+		private static void ReplaceText(CSequence obj)
+		{
+			foreach (var element in obj)
+			{
+				ReplaceText(element);
+			}
+		}
+		private static void ReplaceText(CString obj)
+		{
+			obj.Value = ReplaceText(forbiddenwords, obj.Value);
 		}
 
 		private static string ReplaceText(List<string> forbiddenwords, string text)
@@ -53,5 +99,6 @@ namespace UtilitiesCLI
 
 			return text;
 		}
+
 	}
 }
