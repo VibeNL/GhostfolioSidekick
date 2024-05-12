66cc185f59ec6db62a765b102566bb3a53d95683
VibeNL <30174292+VibeNL@users.noreply.github.com>
2024-05-12 13:13:52 +0200

    getting started on anonymise

---
+++ Tools/AnonymisePDF/Program.cs
@@ -0,0 +1,29 @@
+﻿using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.PdfCleanup;
+using iText.PdfCleanup.Autosweep;
+using System.Text.RegularExpressions;
+
+namespace AnonymisePDF
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var sourceFile = args[0];
+			var targetFile = args[1];
+			var forbiddenWords = args.Skip(2);
+
+			using (var pdf = new PdfDocument(new PdfReader(sourceFile), new PdfWriter(targetFile)))
+			{
+				foreach (var word in forbiddenWords)
+				{
+					var cleanupStrategy = new RegexBasedCleanupStrategy(new Regex(word, RegexOptions.IgnoreCase)).SetRedactionColor(ColorConstants.RED);
+					PdfCleaner.AutoSweepCleanUp(pdf, cleanupStrategy);
+				}
+
+				pdf.Close();
+			}
+		}
+	}
+}
