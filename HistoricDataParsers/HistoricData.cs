03776468085edd91a094dc783b8b2451fc0bcdc8
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-05-30 14:35:40 +0200

    Copied from #192 to avoid merge conflicts

---
+++ HistoricDataParsers/HistoricData.cs
@@ -0,0 +1,20 @@
+﻿
+namespace GhostfolioSidekick.Parsers
+{
+	public class HistoricData
+	{
+		public DateTime Date { get; set; }
+
+		public decimal Open { get; set; }
+
+		public decimal High { get; set; }
+
+		public decimal Low { get; set; }
+
+		public decimal Close { get; set; }
+
+		public decimal Volume { get; set; }
+
+		public required string Symbol { get; set; }
+	}
+}
---
+++ HistoricDataParsers/IHistoryDataFileImporter.cs
@@ -0,0 +1,9 @@
+﻿namespace GhostfolioSidekick.Parsers
+{
+	public interface IHistoryDataFileImporter
+	{
+		Task<bool> CanParseHistoricData(string filename);
+
+		Task<IEnumerable<HistoricData>> ParseHistoricData(string filename);
+	}
+}
