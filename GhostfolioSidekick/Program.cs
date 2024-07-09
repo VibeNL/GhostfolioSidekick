9220dec654215a6b975221ddfe71bd31787afb87
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-07-09 08:45:59 +0200

    Add parser to DI

--- GhostfolioSidekick/Program.cs
+++ GhostfolioSidekick/Program.cs
@@ -127,6 +127,7 @@ namespace GhostfolioSidekick
 							services.AddScoped<IFileImporter, ScalableCapitalPrimeParser>();
 							services.AddScoped<IFileImporter, StockSplitParser>();
 							services.AddScoped<IFileImporter, TradeRepublicInvoiceParserEN>();
+							services.AddScoped<IFileImporter, TradeRepublicInvoiceParserNL>();
 							services.AddScoped<IFileImporter, TradeRepublicStatementParserNL>();
 							services.AddScoped<IFileImporter, Trading212Parser>();
 							
