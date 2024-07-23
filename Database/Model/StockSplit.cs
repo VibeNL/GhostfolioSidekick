2a605ecb2f9f2562012ab5b0a140ca8b412c822f
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-07-23 08:27:04 +0200

    got started on the api

--- Database/Model/StockSplit.cs
+++ Database/Model/StockSplit.cs
@@ -6,9 +6,9 @@
 
 		public required DateOnly Date { get; set; }
 
-		public int FromAmount { get; }
+		public int FromAmount { get; set; }
 
-		public int ToAmount { get; }
+		public int ToAmount { get; set;  }
 
 		public int SymbolProfileId { get; set; }
 	}
--- GhostfolioSidekick/MarketDataMaintainer/AutomatedStockSplitTask.cs
+++ GhostfolioSidekick/MarketDataMaintainer/AutomatedStockSplitTask.cs
@@ -1,5 +1,8 @@
 ﻿using GhostfolioSidekick.Database;
+using GhostfolioSidekick.Database.Model;
+using GhostfolioSidekick.GhostfolioAPI.API;
 using Microsoft.EntityFrameworkCore;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,10 @@ using System.Threading.Tasks;
 
 namespace GhostfolioSidekick.MarketDataMaintainer
 {
-	internal class AutomatedStockSplitTask : IScheduledWork
+	internal class AutomatedStockSplitTask(IRestClient restCallClient) : IScheduledWork
 	{
+		private readonly object FinancialModelingPrepApiKey = "";
+
 		public TaskPriority Priority => TaskPriority.AutomatedStockSplit;
 
 		public TimeSpan ExecutionFrequency => TimeSpan.FromDays(1);
@@ -18,7 +23,50 @@ namespace GhostfolioSidekick.MarketDataMaintainer
 		{
 			var dbContext = await DatabaseContext.GetDatabaseContext();
 
+			foreach (var item in dbContext.SymbolProfiles.Where(x => x.AssetSubClass == Database.Model.AssetSubClass.Stock))
+			{
+				if (item.StockSplitList != null)
+				{
+					continue;
+				}
 
+				// url https://financialmodelingprep.com/api/v3/historical-price-full/stock_split/NVDA?apikey=
+				var url = $"https://financialmodelingprep.com/api/v3/historical-price-full/stock_split/{item.Symbol}?apikey={FinancialModelingPrepApiKey}";
+				var restRequest = new RestRequest(url, Method.Get);
+				var result = await restCallClient.GetAsync<SplitResult>(restRequest);
+
+				if (result != null)
+				{
+					await dbContext.StockSplitLists.AddAsync(new StockSplitList
+					{
+						StockSplits = result.Historical.Select(x => new StockSplit
+						{
+							Date = x.Date,
+							ToAmount = x.Numerator,
+							FromAmount = x.Denominator,
+							SymbolProfileId = item.Id
+						}).ToList(),
+						SymbolProfile = item,
+						SymbolProfileId = item.Id
+					});
+				}
+			}
+
+		}
+
+		public class SplitResult
+		{
+			public required string Symbol { get; set; }
+			public required List<Split> Historical { get; set; }
+		}
+
+		public class Split
+		{
+			public DateOnly Date { get; set; }
+
+			public required int Numerator { get; set; }
+
+			public required int Denominator { get; set; }
 		}
 	}
 }
