92cf0bc7b55e28c3b0bc27cb53b0948f8cdb6662
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-10-20 21:28:26 +0200

    less memory

--- GhostfolioSidekick/MarketDataMaintainer/CurrencyGathererTask.cs
+++ GhostfolioSidekick/MarketDataMaintainer/CurrencyGathererTask.cs
@@ -1,12 +1,15 @@
-﻿using GhostfolioSidekick.Database.Repository;
+﻿using GhostfolioSidekick.Database;
+using GhostfolioSidekick.Database.Repository;
 using GhostfolioSidekick.ExternalDataProvider;
 using GhostfolioSidekick.Model;
 using GhostfolioSidekick.Model.Activities;
+using GhostfolioSidekick.Model.Activities.Types;
 using GhostfolioSidekick.Model.Symbols;
+using Microsoft.EntityFrameworkCore;
 
 namespace GhostfolioSidekick.MarketDataMaintainer
 {
-	internal class CurrencyGathererTask(ICurrencyRepository currencyRepository, IActivityRepository activityRepository, IMarketDataRepository marketDataRepository) : IScheduledWork
+	internal class CurrencyGathererTask(IDbContextFactory<DatabaseContext> databaseContextFactory, ICurrencyRepository currencyRepository) : IScheduledWork
 	{
 		public TaskPriority Priority => TaskPriority.CurrencyGatherer;
 
@@ -14,66 +17,69 @@ namespace GhostfolioSidekick.MarketDataMaintainer
 
 		public async Task DoWork()
 		{
-			var activities = await activityRepository.GetAllActivities();
-			
-			var currencies = new Dictionary<Currency, DateTime>();
-			foreach (var activity in activities.OrderBy(x => x.Date))
-			{
-				switch (activity)
-				{
-					case ActivityWithQuantityAndUnitPrice activityWithQuantityAndUnitPrice:
-						{
-							Currency? key = activityWithQuantityAndUnitPrice.UnitPrice?.Currency;
-							if (key != null && !currencies.ContainsKey(key))
-							{
-								currencies.Add(key, activity.Date);
-							}
-						}
-						break;
-					default:
-						break;
-				}
-			}
+			using var databaseContext = await databaseContextFactory.CreateDbContextAsync();
+			var currencies = (await databaseContext.Activities
+				.OfType<ActivityWithQuantityAndUnitPrice>()
+				.Select(x => new { Currency = x.UnitPrice!.Currency, Date =x.Date })
+				.Distinct()
+				.ToListAsync())
+				.Where(x => x.Currency != null)
+				.GroupBy(x => x.Currency)
+				.Select(x => x.OrderBy(y => y.Date).First());
 
-			var currenciesMatches = currencies.SelectMany((l) => currencies, (l, r) => Tuple.Create(l, r)).Where(x => x.Item1.Key != x.Item2.Key);
+			var currenciesMatches = currencies.SelectMany((l) => currencies, (l, r) => Tuple.Create(l, r)).Where(x => x.Item1.Currency != x.Item2.Currency);
 
 			foreach (var match in currenciesMatches)
 			{
-				if (match.Item1.Key.IsKnownPair(match.Item2.Key))
+				if (match.Item1.Currency.IsKnownPair(match.Item2.Currency))
 				{
 					continue;
 				}
 
-				string symbolString = match.Item1.Key.Symbol + match.Item2.Key.Symbol;
-				DateOnly fromDate = DateOnly.FromDateTime(new DateTime[] { match.Item1.Value, match.Item2.Value }.Min());
+				string symbolString = match.Item1.Currency.Symbol + match.Item2.Currency.Symbol;
+				DateOnly fromDate = DateOnly.FromDateTime(new DateTime[] { match.Item1.Date, match.Item2.Date }.Min());
 
-				var symbol = await marketDataRepository.GetSymbolProfileBySymbol(symbolString);
-				
+				var dates = await databaseContext.SymbolProfiles
+					.Where(x => x.Symbol == symbolString)
+					.SelectMany(x => x.MarketData)
+					.GroupBy(x => 1)
+					.Select(g => new
+					{
+						MinDate = g.Min(x => x.Date),
+						MaxDate = g.Max(x => x.Date)
+					})
+					.FirstOrDefaultAsync();
+
 				// Check if we need to update our data
-				if (symbol != null && symbol.MarketData.Count > 0 &&
-						DateOnly.FromDateTime(symbol.MarketData.Min(x => x.Date)) == fromDate &&
-						symbol.MarketData.Max(x => x.Date) == DateTime.Today)
+				if (dates != null &&
+						DateOnly.FromDateTime(dates.MinDate) == fromDate &&
+						dates.MaxDate == DateTime.Today)
 				{
 					continue;
 				}
 
-				var currencyHistory = await currencyRepository.GetCurrencyHistory(match.Item1.Key, match.Item2.Key, fromDate);
+				var currencyHistory = await currencyRepository.GetCurrencyHistory(match.Item1.Currency, match.Item2.Currency, fromDate);
 				if (currencyHistory != null)
 				{
-					symbol = await marketDataRepository.GetSymbolProfileBySymbol(symbolString);
-					if (symbol == null)
-					{
-						symbol = new SymbolProfile(symbolString, symbolString, [], match.Item1.Key with {}, Datasource.YAHOO, AssetClass.Undefined, null, [], []);
-					}
+					using var writeDatabaseContext = await databaseContextFactory.CreateDbContextAsync();
 
-					symbol.MarketData.Clear();
+					var symbolProfile = await writeDatabaseContext.SymbolProfiles
+						.Where(x => x.Symbol == symbolString)
+						.FirstOrDefaultAsync() ?? new SymbolProfile(symbolString, symbolString, [], match.Item1.Currency with { }, Datasource.YAHOO, AssetClass.Undefined, null, [], []);
+					symbolProfile.MarketData.Clear();
 
 					foreach (var item in currencyHistory)
 					{
-						symbol.MarketData.Add(item);
+						symbolProfile.MarketData.Add(item);
 					}
 
-					await marketDataRepository.Store(symbol).ConfigureAwait(false);
+
+					if (!await databaseContext.SymbolProfiles.ContainsAsync(symbolProfile).ConfigureAwait(false))
+					{
+						await databaseContext.SymbolProfiles.AddAsync(symbolProfile);
+					}
+
+					await databaseContext.SaveChangesAsync();
 				}
 			}
 		}
