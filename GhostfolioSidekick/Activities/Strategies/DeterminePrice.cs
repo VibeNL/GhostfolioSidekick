b1b5aec500b9e9ea6808709a220f6e04973572d1
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-10-31 12:01:52 +0100

    Added determine price

---
+++ GhostfolioSidekick/Activities/Strategies/DeterminePrice.cs
@@ -0,0 +1,60 @@
+﻿using GhostfolioSidekick.Model;
+using GhostfolioSidekick.Model.Activities;
+using GhostfolioSidekick.Model.Activities.Types;
+using GhostfolioSidekick.Model.Symbols;
+
+namespace GhostfolioSidekick.Activities.Strategies
+{
+	public class DeterminePrice : IHoldingStrategy
+	{
+		public int Priority => (int)StrategiesPriority.DeterminePrice;
+
+		public Task Execute(Holding holding)
+		{
+			if (holding.SymbolProfiles.Count != 0)
+			{
+				return Task.CompletedTask;
+			}
+
+			var activities = holding.Activities
+				.OrderBy(x => x.Date).ToList();
+
+			if (activities.Count == 0)
+			{
+				return Task.CompletedTask;
+			}
+
+			foreach (var activity in activities.OfType<ActivityWithQuantityAndUnitPrice>())
+			{
+				switch (activity)
+				{
+					case SendAndReceiveActivity:
+					case GiftActivity:
+					case StakingRewardActivity:
+						SetUnitPrice(holding.SymbolProfiles, activity);
+						break;
+					default:
+						break;
+				}
+			}
+
+			return Task.CompletedTask;
+		}
+
+		private static void SetUnitPrice(List<SymbolProfile> symbolProfiles, ActivityWithQuantityAndUnitPrice activity)
+		{
+			foreach (var symbolProfile in symbolProfiles)
+			{
+				var marketData = symbolProfile
+									.MarketData
+									.OrderBy(x => x.Date)
+									.FirstOrDefault(x => x.Date >= DateOnly.FromDateTime(activity.Date));
+				if (marketData != null)
+				{
+					activity.AdjustedUnitPrice = marketData.Close;
+					activity.AdjustedUnitPriceSource.Add(new CalculatedPriceTrace("Determine price", activity.AdjustedQuantity, activity.AdjustedUnitPrice));
+				}
+			}
+		}
+	}
+}
