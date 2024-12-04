3bd80f52b1969bc2feb6488d42810d73e596daa2
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-12-04 10:42:26 +0100

    more tests

---
+++ Database.UnitTests/DatabaseContextTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using GhostfolioSidekick.Database;
+using KellermanSoftware.CompareNetObjects;
+using FluentAssertions;
+
+namespace GhostfolioSidekick.Tests
+{
+	public class DatabaseContextTests
+	{
+		public DatabaseContextTests()
+		{
+
+		}
+
+		[Fact]
+		public async Task CanApplyMigrations()
+		{
+			// Arrange
+			var options = new DbContextOptionsBuilder<DatabaseContext>()
+				.UseSqlite("Data Source=:memory:")
+				.Options;
+
+			using (var context = new DatabaseContext(options))
+			{
+				context.Database.OpenConnection();
+
+				// Act
+				await context.Database.MigrateAsync().ConfigureAwait(false);
+
+				// Assert
+				var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+				pendingMigrations.Should().BeEmpty();
+
+				// Check if table Holding exists
+				var tableExists = await context.Database.ExecuteSqlRawAsync("SELECT name FROM sqlite_master WHERE type='table' AND name='Holdings';");
+			}
+		}
+	}
+}
