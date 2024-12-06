05586cfed7d108cd963f5e39eebe3e133e35d593
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2024-12-06 20:57:50 +0100

    more tests

---
+++ GhostfolioAPI.UnitTests/API/Mapper/ContractToModelMapperTests.cs
@@ -0,0 +1,99 @@
+using FluentAssertions;
+using GhostfolioSidekick.GhostfolioAPI.API.Mapper;
+using GhostfolioSidekick.Model.Activities;
+
+namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API.Mapper
+{
+	public class ContractToModelMapperTests
+	{
+		[Fact]
+		public void MapPlatform_ShouldMapCorrectly()
+		{
+			// Arrange
+			var rawPlatform = new Contract.Platform
+			{
+				Name = "Test Platform",
+				Url = "http://testplatform.com",
+				Id = Guid.NewGuid().ToString()
+			};
+
+			// Act
+			var result = ContractToModelMapper.MapPlatform(rawPlatform);
+
+			// Assert
+			result.Name.Should().Be("Test Platform");
+			result.Url.Should().Be("http://testplatform.com");
+		}
+
+		[Fact]
+		public void MapAccount_ShouldMapCorrectly()
+		{
+			// Arrange
+			var rawAccount = new Contract.Account
+			{
+				Name = "Test Account",
+				Comment = "Test Comment",
+				Currency = "USD",
+				Balance = 1000m,
+				Id = Guid.NewGuid().ToString()
+			};
+			var rawPlatform = new Contract.Platform
+			{
+				Name = "Test Platform",
+				Url = "http://testplatform.com",
+				Id = Guid.NewGuid().ToString()
+			};
+
+			// Act
+			var result = ContractToModelMapper.MapAccount(rawAccount, rawPlatform);
+
+			// Assert
+			result.Name.Should().Be("Test Account");
+			result.Comment.Should().Be("Test Comment");
+			result.Platform.Should().NotBeNull();
+			result.Platform!.Name.Should().Be("Test Platform");
+			result.Balance.Should().HaveCount(1);
+			result.Balance.First().Money.Amount.Should().Be(1000m);
+		}
+
+		[Fact]
+		public void MapSymbolProfile_ShouldMapCorrectly()
+		{
+			// Arrange
+			var rawSymbolProfile = new Contract.SymbolProfile
+			{
+				Symbol = "AAPL",
+				Name = "Apple Inc.",
+				Currency = "USD",
+				DataSource = "Yahoo",
+				AssetClass = "EQUITY",
+				AssetSubClass = "STOCK",
+				ISIN = "US0378331005",
+				Comment = "Test Comment",
+				Countries = new[]
+				{
+					new Contract.Country { Name = "United States", Code = "US", Continent = "North America", Weight = 100m }
+				},
+				Sectors = new[]
+				{
+					new Contract.Sector { Name = "Technology", Weight = 100m }
+				}
+			};
+
+			// Act
+			var result = ContractToModelMapper.MapSymbolProfile(rawSymbolProfile);
+
+			// Assert
+			result.Symbol.Should().Be("AAPL");
+			result.Name.Should().Be("Apple Inc.");
+			result.Currency.Symbol.Should().Be("USD");
+			result.DataSource.Should().Be("GHOSTFOLIO_Yahoo");
+			result.AssetClass.Should().Be(AssetClass.Equity);
+			result.AssetSubClass.Should().Be(AssetSubClass.Stock);
+			result.ISIN.Should().Be("US0378331005");
+			result.Comment.Should().Be("Test Comment");
+			result.CountryWeight.Should().HaveCount(1);
+			result.SectorWeights.Should().HaveCount(1);
+		}
+	}
+}
---
+++ GhostfolioAPI.UnitTests/API/Mapper/ModelToContractMapperTests.cs
@@ -0,0 +1,219 @@
+using AutoFixture;
+using FluentAssertions;
+using GhostfolioSidekick.Database.Repository;
+using GhostfolioSidekick.GhostfolioAPI.API.Mapper;
+using GhostfolioSidekick.GhostfolioAPI.Contract;
+using GhostfolioSidekick.Model;
+using GhostfolioSidekick.Model.Activities.Types;
+using Moq;
+
+namespace GhostfolioSidekick.GhostfolioAPI.UnitTests.API.Mapper
+{
+	public class ModelToContractMapperTests
+	{
+		private readonly IFixture _fixture;
+		private readonly Mock<ICurrencyExchange> _exchangeRateServiceMock;
+
+		public ModelToContractMapperTests()
+		{
+			_fixture = new Fixture();
+			_fixture.Customize<DateOnly>(o => o.FromFactory((DateTime dt) => DateOnly.FromDateTime(dt)));
+			_fixture.Customize<TimeOnly>(o => o.FromFactory((DateTime dt) => TimeOnly.FromDateTime(dt)));
+
+			_exchangeRateServiceMock = new Mock<ICurrencyExchange>();
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapBuySellActivityCorrectly()
+		{
+			// Arrange
+			var buyActivity = _fixture.Build<BuySellActivity>().Without(x => x.Holding).Create();
+			var symbolProfile = _fixture.Create<SymbolProfile>();
+			var account = _fixture.Create<Account>();
+
+			_exchangeRateServiceMock
+				.Setup(x => x.ConvertMoney(It.IsAny<Money>(), It.IsAny<Currency>(), It.IsAny<DateOnly>()))
+				.ReturnsAsync(new Money(new Currency(symbolProfile.Currency), 100m));
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, symbolProfile, buyActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Should().Be(symbolProfile);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(buyActivity.Date);
+			result.Fee.Should().Be(600m); // Assuming fees and taxes are converted to 100 each
+			result.FeeCurrency.Should().Be(symbolProfile.Currency);
+			result.Quantity.Should().Be(Math.Abs(buyActivity.AdjustedQuantity.GetValueOrDefault()));
+			result.Type.Should().Be(buyActivity.AdjustedQuantity > 0 ? ActivityType.BUY : ActivityType.SELL);
+			result.UnitPrice.Should().Be(100m);
+			result.ReferenceCode.Should().Be(buyActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapSendAndReceiveActivityCorrectly()
+		{
+			// Arrange
+			var sendAndReceiveActivity = _fixture.Build<SendAndReceiveActivity>().Without(x => x.Holding).Create();
+			var symbolProfile = _fixture.Create<SymbolProfile>();
+			var account = _fixture.Create<Account>();
+
+			_exchangeRateServiceMock
+				.Setup(x => x.ConvertMoney(It.IsAny<Money>(), It.IsAny<Currency>(), It.IsAny<DateOnly>()))
+				.ReturnsAsync(new Money(new Currency(symbolProfile.Currency), 100m));
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, symbolProfile, sendAndReceiveActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Should().Be(symbolProfile);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(sendAndReceiveActivity.Date);
+			result.Fee.Should().Be(300m); // Assuming fees are converted to 100
+			result.FeeCurrency.Should().Be(symbolProfile.Currency);
+			result.Quantity.Should().Be(Math.Abs(sendAndReceiveActivity.AdjustedQuantity.GetValueOrDefault()));
+			result.Type.Should().Be(sendAndReceiveActivity.AdjustedQuantity > 0 ? ActivityType.BUY : ActivityType.SELL);
+			result.UnitPrice.Should().Be(100m);
+			result.ReferenceCode.Should().Be(sendAndReceiveActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapDividendActivityCorrectly()
+		{
+			// Arrange
+			var dividendActivity = _fixture.Build<DividendActivity>().Without(x => x.Holding).Create();
+			var symbolProfile = _fixture.Create<SymbolProfile>();
+			var account = _fixture.Create<Account>();
+
+			_exchangeRateServiceMock
+				.Setup(x => x.ConvertMoney(It.IsAny<Money>(), It.IsAny<Currency>(), It.IsAny<DateOnly>()))
+				.ReturnsAsync(new Money(new Currency(symbolProfile.Currency), 100m));
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, symbolProfile, dividendActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Should().Be(symbolProfile);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(dividendActivity.Date);
+			result.Fee.Should().Be(600m); // Assuming fees and taxes are converted to 100 each
+			result.FeeCurrency.Should().Be(symbolProfile.Currency);
+			result.Quantity.Should().Be(1);
+			result.Type.Should().Be(ActivityType.DIVIDEND);
+			result.UnitPrice.Should().Be(100m);
+			result.ReferenceCode.Should().Be(dividendActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapInterestActivityCorrectly()
+		{
+			// Arrange
+			var interestActivity = _fixture.Build<InterestActivity>().Without(x => x.Holding).Create();
+			var account = _fixture.Create<Account>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, null, interestActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Currency.Should().Be(interestActivity.Amount.Currency.Symbol);
+			result.SymbolProfile.Name.Should().Be(interestActivity.Description);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(interestActivity.Date);
+			result.Quantity.Should().Be(1);
+			result.Type.Should().Be(ActivityType.INTEREST);
+			result.UnitPrice.Should().Be(interestActivity.Amount.Amount);
+			result.ReferenceCode.Should().Be(interestActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapFeeActivityCorrectly()
+		{
+			// Arrange
+			var feeActivity = _fixture.Build<FeeActivity>().Without(x => x.Holding).Create();
+			var account = _fixture.Create<Account>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, null, feeActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Currency.Should().Be(feeActivity.Amount.Currency.Symbol);
+			result.SymbolProfile.Name.Should().Be(feeActivity.Description);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(feeActivity.Date);
+			result.Quantity.Should().Be(1);
+			result.Type.Should().Be(ActivityType.FEE);
+			result.UnitPrice.Should().Be(feeActivity.Amount.Amount);
+			result.ReferenceCode.Should().Be(feeActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapValuableActivityCorrectly()
+		{
+			// Arrange
+			var valuableActivity = _fixture.Build<ValuableActivity>().Without(x => x.Holding).Create();
+			var account = _fixture.Create<Account>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, null, valuableActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Currency.Should().Be(valuableActivity.Price.Currency.Symbol);
+			result.SymbolProfile.Name.Should().Be(valuableActivity.Description);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(valuableActivity.Date);
+			result.Quantity.Should().Be(1);
+			result.Type.Should().Be(ActivityType.ITEM);
+			result.UnitPrice.Should().Be(valuableActivity.Price.Amount);
+			result.ReferenceCode.Should().Be(valuableActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldMapLiabilityActivityCorrectly()
+		{
+			// Arrange
+			var liabilityActivity = _fixture.Build<LiabilityActivity>().Without(x => x.Holding).Create();
+			var account = _fixture.Create<Account>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, null, liabilityActivity, account);
+
+			// Assert
+			result.Should().NotBeNull();
+			result!.SymbolProfile.Currency.Should().Be(liabilityActivity.Price.Currency.Symbol);
+			result.SymbolProfile.Name.Should().Be(liabilityActivity.Description);
+			result.Comment.Should().NotBeNull();
+			result.Date.Should().Be(liabilityActivity.Date);
+			result.Quantity.Should().Be(1);
+			result.Type.Should().Be(ActivityType.LIABILITY);
+			result.UnitPrice.Should().Be(liabilityActivity.Price.Amount);
+			result.ReferenceCode.Should().Be(liabilityActivity.TransactionId);
+			result.AccountId.Should().Be(account.Id);
+		}
+
+		[Fact]
+		public async Task ConvertToGhostfolioActivity_ShouldReturnNullForUnsupportedActivity()
+		{
+			// Arrange
+			var unsupportedActivity = _fixture.Build<KnownBalanceActivity>().Without(x => x.Holding).Create();
+			var account = _fixture.Create<Account>();
+
+			// Act
+			var result = await ModelToContractMapper.ConvertToGhostfolioActivity(_exchangeRateServiceMock.Object, null, unsupportedActivity, account);
+
+			// Assert
+			result.Should().BeNull();
+		}
+	}
+}
