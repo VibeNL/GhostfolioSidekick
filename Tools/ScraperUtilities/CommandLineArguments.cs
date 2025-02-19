1873097abd3bcec1fc5977ef4764e0afc4c2ec6c
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2025-02-19 09:58:23 +0100

    remove portfolio option from the commandline

--- Tools/ScraperUtilities/CommandLineArguments.cs
+++ Tools/ScraperUtilities/CommandLineArguments.cs
@@ -6,17 +6,9 @@
 		string Password, 
 		string OutputFile)
 	{
-		public string Portfolio { get; internal set; }
-
 		internal static CommandLineArguments Parse(string[] args)
 		{
 			var c = new CommandLineArguments(args[0], args[1], args[2], args[3]);
-
-			if (args.Length > 4)
-			{
-				c.Portfolio = args[4];
-			}
-
 			return c;
 		}
 	}
