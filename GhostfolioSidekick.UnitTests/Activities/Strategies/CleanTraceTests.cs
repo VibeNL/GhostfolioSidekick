1efaa60a1235f5d3173ee677a4f84d6cc3478eb6
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2025-03-06 14:23:44 +0100

    some changes

---
+++ GhostfolioSidekick.UnitTests/Activities/Strategies/CleanTraceTests.cs
@@ -0,0 +1,49 @@
+using FluentAssertions;
+using GhostfolioSidekick.Activities.Strategies;
+using GhostfolioSidekick.Model;
+using GhostfolioSidekick.Model.Activities;
+using Moq;
+
+namespace GhostfolioSidekick.Tests.Activities.Strategies
+{
+    public class CleanTraceTests
+    {
+        [Fact]
+        public async Task Execute_NoActivities_DoesNothing()
+        {
+            // Arrange
+            var holding = new Holding
+            {
+                Activities = new List<Activity>()
+            };
+            var cleanTrace = new CleanTrace();
+
+            // Act
+            await cleanTrace.Execute(holding);
+
+            // Assert
+            holding.Activities.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Execute_WithActivities_CleansAdjustedUnitPriceSource()
+        {
+            // Arrange
+            var activity = new Mock<ActivityWithQuantityAndUnitPrice>();
+            var adjustedUnitPriceSource = new List<CalculatedPriceTrace> { new CalculatedPriceTrace() };
+            activity.Setup(a => a.AdjustedUnitPriceSource).Returns(adjustedUnitPriceSource);
+
+            var holding = new Holding
+            {
+                Activities = new List<Activity> { activity.Object }
+            };
+            var cleanTrace = new CleanTrace();
+
+            // Act
+            await cleanTrace.Execute(holding);
+
+            // Assert
+            adjustedUnitPriceSource.Should().BeEmpty();
+        }
+    }
+}
