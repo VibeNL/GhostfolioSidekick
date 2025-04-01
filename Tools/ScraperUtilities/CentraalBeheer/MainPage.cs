a28e202e6890de8743d406e9243da7971c98f6b1
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2025-04-01 14:22:13 +0200

    fixing getting to the transaction page

--- Tools/ScraperUtilities/CentraalBeheer/MainPage.cs
+++ Tools/ScraperUtilities/CentraalBeheer/MainPage.cs
@@ -1,20 +1,42 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using Microsoft.Extensions.Logging;
 using Microsoft.Playwright;
 
 namespace ScraperUtilities.CentraalBeheer
 {
-	public class MainPage(IPage page, Microsoft.Extensions.Logging.ILogger logger)
-	{
-		internal async Task<TransactionPage> GoToTransactions()
-		{
-			// 
-			await page.GotoAsync("https://www.centraalbeheer.nl/mijncb/mijn-producten/beleggen/transacties");
+    public class MainPage(IPage page, Microsoft.Extensions.Logging.ILogger logger)
+    {
+        internal async Task<TransactionPage> GoToTransactions()
+        {
+            // Navigate to pages
+            await page.GotoAsync("https://www.centraalbeheer.nl/mijncb/mijn-producten");
+            await page.GotoAsync("https://www.centraalbeheer.nl/mijncb/mijn-producten/beleggen");
 
-			// Wait for transactions to load
-			logger.LogInformation("Waiting for transactions to load...");
-			await page.WaitForSelectorAsync("#transacties", new PageWaitForSelectorOptions { State = WaitForSelectorState.Visible });
+            // find button "Bekijken" and press it
+            await PressButton("Bekijken");
 
-			return new TransactionPage(page, logger);
-		}
-	}
+			// Find button "Äfgeronde opdrachten"
+			await PressButton("Afgeronde opdrachten");
+
+            // Wait for transactions to load
+            logger.LogInformation("Waiting for transactions to load...");
+            await page.WaitForSelectorAsync("#transacties", new PageWaitForSelectorOptions { State = WaitForSelectorState.Visible });
+
+            return new TransactionPage(page, logger);
+        }
+
+        private async Task PressButton(string text)
+        {
+            var button = page.Locator($"//a[contains(text(),\"{text}\")]");
+            if (button != null)
+            {
+                await button.ClickAsync();
+            }
+            else
+            {
+                logger.LogError("Button '{ButtonText}' not found.", text);
+                throw new Exception($"Button '{text}' not found.");
+            }
+        }
+    }
 }
