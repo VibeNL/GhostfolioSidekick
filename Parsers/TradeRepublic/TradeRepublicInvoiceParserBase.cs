615ac483d22421759e41d2ebd28a759b9c84bead
VibeNL <30174292+VibeNL@users.noreply.github.com>
2025-04-03 20:07:05 +0200

    Add Trade Republic parser support for Spanish files (#423)
    * Test files added
    
    * First tests added
    
    * First implementation
    
    * more green
    
    * way too complex parsing
    
    * i quit for today
    
    * another green test
    
    * Fixing last test
    
    * New scenario added

--- Parsers/TradeRepublic/TradeRepublicInvoiceParserBase.cs
+++ Parsers/TradeRepublic/TradeRepublicInvoiceParserBase.cs
@@ -11,14 +11,15 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 	{
 		protected abstract string Keyword_Position { get; }
 		protected abstract string Keyword_Quantity { get; }
-		protected abstract string Keyword_Price { get; }
+		protected abstract string Keyword_Quantity_PiecesText { get; }
+		protected abstract string[] Keyword_Price { get; }
 		protected abstract string Keyword_Amount { get; }
-		protected abstract string Keyword_Nominal { get; }
+		protected abstract string[] Keyword_Nominal { get; }
 		protected abstract string Keyword_Income { get; }
 		protected abstract string Keyword_Coupon { get; }
 		protected abstract string Keyword_Total { get; }
 		protected abstract string Keyword_AverageRate { get; }
-		protected abstract string Keyword_Booking { get; }
+		protected abstract string[] Keyword_Booking { get; }
 		protected abstract string Keyword_Security { get; }
 		protected abstract string Keyword_Number { get; }
 		protected abstract string SECURITIES_SETTLEMENT { get; }
@@ -29,25 +30,25 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 		protected abstract string EXTERNAL_COST_SURCHARGE { get; }
 		protected abstract string WITHHOLDING_TAX { get; }
 		protected abstract string DATE { get; }
-		protected abstract CultureInfo CULTURE { get; }
+		protected abstract CultureInfo Culture { get; }
+		protected CultureInfo AlternativeCulture{ get; } = CultureInfo.InvariantCulture;
 
 		private List<string> TableKeyWords
 		{
 			get
 			{
-				return [
+				return [.. Keyword_Booking.Union(Keyword_Nominal)
+				.Union(Keyword_Price)	
+				.Union([
 					Keyword_Position,
 					Keyword_Quantity,
-					Keyword_Nominal,
-					Keyword_Price,
 					Keyword_AverageRate,
 					Keyword_Income,
-					Keyword_Coupon,
 					Keyword_Amount,
-					Keyword_Booking,
+					Keyword_Coupon,
 					Keyword_Security,
 					Keyword_Number
-				];
+				])];
 			}
 		}
 
@@ -92,6 +93,8 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 		{
 			var activities = new List<PartialActivity>();
 
+			var hasMainActivityParsed = false;
+
 			// detect headers
 			var headers = new List<MultiWordToken>();
 			DateTime? dateTime = null;
@@ -116,7 +119,11 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 
 				if (!inHeader && headers.Count == 4) // parsing rows buys and sells
 				{
-					i = ParseSecurityRecord(words, i, dateTime.GetValueOrDefault(), headers, activities);
+					if (!hasMainActivityParsed)
+					{
+						i = ParseSecurityRecord(words, i, dateTime.GetValueOrDefault(), headers, activities);
+						hasMainActivityParsed = true;
+					}
 				}
 
 				if (!inHeader && headers.Count == 2) // parsing fees
@@ -132,7 +139,7 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 				if (inHeader) // add column headers
 				{
 					var matched = false;
-					foreach (var kw in TableKeyWords)
+					foreach (var kw in TableKeyWords.OrderByDescending(x => x.Length))
 					{
 						var keywordMatch = true;
 						string[] keywordSplitted = kw.Split(" ");
@@ -162,7 +169,8 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 					}
 				}
 
-				if (Keyword_Amount == word.Text) // end of header
+				if (Keyword_Amount.Contains(word.Text) || 
+					(headers.Count != 0 && CheckEndOfRecord(headers, word.Text))) // end of header
 				{
 					inHeader = false;
 				}
@@ -172,6 +180,11 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 			activities.ToList().ForEach(x => x.TransactionId = mainActivity.TransactionId);
 
 			return activities;
+		}
+
+		protected virtual bool CheckEndOfRecord(List<MultiWordToken> headers, string currentWord)
+		{
+			return false;
 		}
 
 		private int ParseFeeRecords(List<SingleWordToken> words, int i, DateTime dateTime, List<PartialActivity> activities)
@@ -193,8 +206,8 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 			{
 				return i;
 			}
-			
-			var price = Math.Abs(decimal.Parse(words[i + skip].Text, CULTURE));
+
+			var price = Math.Abs(Parse(words[i + skip].Text));
 			var currencySymbol = words[i + skip + 1].Text;
 			var currency = Currency.GetCurrency(currencySymbol);
 
@@ -211,24 +224,28 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 		private int ParseSecurityRecord(List<SingleWordToken> words, int i, DateTime dateTime, List<MultiWordToken> headers, List<PartialActivity> activities)
 		{
 			var headerStrings = headers.Select(h => h.KeyWord).ToList();
-			if (headerStrings.Contains(Keyword_Quantity) && (headerStrings.Contains(Keyword_Price) || headerStrings.Contains(Keyword_AverageRate))) // Stocks
+			if (headerStrings.Contains(Keyword_Quantity) && (Keyword_Price.Any(x => headerStrings.Contains(x)) || headerStrings.Contains(Keyword_AverageRate))) // Stocks
 			{
 				var isin = GetIsin(words, ref i);
 				string id = GetId(dateTime, isin);
 
+				var incrementDueToPiecesText = words[i + 2].Text == Keyword_Quantity_PiecesText ? 1 : 0;
+
 				activities.Add(PartialActivity.CreateBuy(
-					Currency.GetCurrency(words[i + 4].Text),
+					Currency.GetCurrency(words[i + 3 + incrementDueToPiecesText].Text),
 					dateTime,
 					[PartialSymbolIdentifier.CreateStockBondAndETF(isin)],
-					decimal.Parse(words[i + 1].Text, CULTURE),
-					decimal.Parse(words[i + 3].Text, CULTURE),
-					new Money(Currency.GetCurrency(words[i + 4].Text), decimal.Parse(words[i + 5].Text, CULTURE)),
+					Parse(words[i + 1].Text),
+					Parse(words[i + 2 + incrementDueToPiecesText].Text),
+					new Money(
+						Currency.GetCurrency(words[i + 3 + incrementDueToPiecesText].Text), 
+						Parse(words[i + 4 + incrementDueToPiecesText].Text)),
 					id));
 
-				return i + 6;
+				return i + 5 + incrementDueToPiecesText;
 			}
 
-			if (headerStrings.Contains(Keyword_Nominal) && headerStrings.Contains(Keyword_Price)) // Bonds
+			if (Keyword_Nominal.Any(x => headerStrings.Contains(x)) && Keyword_Price.Any(x => headerStrings.Contains(x))) // Bonds
 			{
 				var isin = GetIsin(words, ref i);
 				string id = GetId(dateTime, isin);
@@ -237,15 +254,15 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 					Currency.GetCurrency(words[i + 6].Text),
 					dateTime,
 					[PartialSymbolIdentifier.CreateStockBondAndETF(isin)],
-					decimal.Parse(words[i + 1].Text, CULTURE),
-					decimal.Parse(words[i + 3].Text, CULTURE) / 100,
-					new Money(Currency.GetCurrency(words[i + 6].Text), decimal.Parse(words[i + 5].Text, CULTURE)),
+					Parse(words[i + 1].Text),
+					Parse(words[i + 3].Text) / 100,
+					new Money(Currency.GetCurrency(words[i + 6].Text), Parse(words[i + 5].Text)),
 					id));
 
 				return i + 6;
 			}
 
-			if (headerStrings.Contains(Keyword_Income) || headerStrings.Contains(Keyword_Nominal)) // Dividends
+			if (headerStrings.Contains(Keyword_Income) || Keyword_Nominal.Any(x => headerStrings.Contains(x))) // Dividends
 			{
 				var isin = GetIsin(words, ref i);
 				string id = GetId(dateTime, isin);
@@ -254,14 +271,14 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 					Currency.GetCurrency(words[i + 6].Text),
 					dateTime,
 					[PartialSymbolIdentifier.CreateStockBondAndETF(isin)],
-					decimal.Parse(words[i + 5].Text, CULTURE),
-					new Money(Currency.GetCurrency(words[i + 6].Text), decimal.Parse(words[i + 5].Text, CULTURE)),
+					Parse(words[i + 5].Text	),
+					new Money(Currency.GetCurrency(words[i + 6].Text), Parse(words[i + 5].Text)),
 					id));
 
 				return i + 6;
 			}
 
-			if (headerStrings.Contains(Keyword_Security) && headerStrings.Contains(Keyword_Booking)) // Repay of bonds
+			if (headerStrings.Contains(Keyword_Security) && Keyword_Booking.Any(x => headerStrings.Contains(x))) // Repay of bonds
 			{
 				i = i + 2; // skip column "Number" and "Booking"
 				var isin = GetIsin(words, ref i);
@@ -271,8 +288,8 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 					Currency.GetCurrency(words[i + 2].Text),
 					dateTime,
 					[PartialSymbolIdentifier.CreateStockBondAndETF(isin)],
-					decimal.Parse(words[i + 1].Text, CULTURE),
-					new Money(Currency.GetCurrency(words[i + 2].Text), decimal.Parse(words[i + 1].Text, CULTURE)),
+					Parse(words[i + 1].Text),
+					new Money(Currency.GetCurrency(words[i + 2].Text), Parse(words[i + 1].Text)),
 					id));
 
 				return i + 2;
@@ -316,6 +333,29 @@ namespace GhostfolioSidekick.Parsers.TradeRepublic
 			}
 
 			throw new NotSupportedException("ISIN not found");
+		}
+
+		private decimal Parse(string s)
+		{
+			// Check if the string contains both decimal separators. Replace the first of the two with an empty string.
+			var indexOfComma = s.IndexOf(',');
+			var indexOfDots = s.IndexOf('.');
+			if (indexOfComma != -1 && indexOfDots != -1)
+			{
+				if (indexOfComma < indexOfDots)
+				{
+					s = s.Replace(",", string.Empty);
+				}
+				else
+				{
+					s = s.Replace(".", string.Empty);
+				}
+			}
+
+			var a = decimal.Parse(s, Culture);
+			var b = decimal.Parse(s, AlternativeCulture);
+
+			return Math.Abs(a) < Math.Abs(b) ? a : b;
 		}
 	}
 }
