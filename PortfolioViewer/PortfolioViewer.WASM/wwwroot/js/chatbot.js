1bd10506b2cb1737dc74a930b17617b5985a06b1
Vincent Berkien <30174292+VibeNL@users.noreply.github.com>
2025-04-17 08:07:56 +0200

    first draft of UI

---
+++ PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/chatbot.js
@@ -0,0 +1,9 @@
+﻿// Basic example assuming webllm.js is loaded and model is initialized
+window.webllmChat = async function (inputText) {
+    if (!window.myWebLLM) {
+        window.myWebLLM = await webllm.createChatWorker();
+        await window.myWebLLM.load("Llama-3-8B-Instruct-q4f32_1");
+    }
+    const reply = await window.myWebLLM.chat.completion(inputText);
+    return reply.message.content;
+};
