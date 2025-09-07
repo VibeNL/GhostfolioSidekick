a470ddeda53ff56b12157091cdc455534e671462
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-09-07 13:31:34 +0200

    remove more -1

--- Tools/ScraperUtilities/Program.cs
+++ Tools/ScraperUtilities/Program.cs
@@ -122,7 +122,7 @@ namespace GhostfolioSidekick.Tools.ScraperUtilities
 						Symbol = activity.Symbol,
 						Date = sellActivity.Date,
 						Currency = sellActivity.UnitPrice.Currency.Symbol,
-						Quantity = sellActivity.Quantity * -1,
+						Quantity = sellActivity.Quantity,
 						UnitPrice = sellActivity.UnitPrice.Amount,
 						Fee = Sum(sellActivity.Fees.Select(x => x.Money)),
 						Tax = Sum(sellActivity.Taxes.Select(x => x.Money)),
@@ -168,7 +168,7 @@ namespace GhostfolioSidekick.Tools.ScraperUtilities
 						Date = withdrawal.Date,
 						Currency = withdrawal.Amount.Currency.Symbol,
 						Quantity = 1,
-						UnitPrice = withdrawal.Amount.Amount * -1,
+						UnitPrice = withdrawal.Amount.Amount,
 						Fee = 0,
 						Tax = 0,
 					};
