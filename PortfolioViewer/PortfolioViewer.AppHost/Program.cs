a9761c0e75cc1aedcc6f0b963daf72de33b12a7f
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-10-15 12:39:47 +0200

    Remove the unused local variable 'webfrontend'.

--- PortfolioViewer/PortfolioViewer.AppHost/Program.cs
+++ PortfolioViewer/PortfolioViewer.AppHost/Program.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 // https://github.com/BenjaminCharlton/Aspire4Wasm
 
 var apiService = builder.AddProject<Projects.PortfolioViewer_ApiService>("apiservice");
-var webfrontend = builder.AddProject<Projects.PortfolioViewer_WASM>("blazorServer")
+builder.AddProject<Projects.PortfolioViewer_WASM>("blazorServer")
 						.AddWebAssemblyClient<Projects.PortfolioViewer_WASM>("webfrontend")
 						.WithReference(apiService);
 
