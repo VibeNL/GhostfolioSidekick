a956a05736160474f7a7dfce000e145a8ec94c99
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-10-27 20:12:26 +0100

    Split classes to own file

---
+++ AI/AI.Agents/SearchResultItem.cs
@@ -0,0 +1,9 @@
+﻿namespace GhostfolioSidekick.AI.Agents
+{
+	public class SearchResultItem
+	{
+		public string Title { get; set; } = string.Empty;
+		public string Link { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+	}
+}
---
+++ AI/AI.Agents/SearchResults.cs
@@ -0,0 +1,8 @@
+﻿namespace GhostfolioSidekick.AI.Agents
+{
+	public class SearchResults
+	{
+		public string Query { get; set; } = string.Empty;
+		public List<SearchResultItem> Items { get; set; } = [];
+	}
+}
