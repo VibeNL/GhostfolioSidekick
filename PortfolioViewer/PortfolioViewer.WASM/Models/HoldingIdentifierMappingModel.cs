08082c46f87a66800da11a1099e505e24833e657
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-11-05 10:41:00 +0100

    cleanup

--- PortfolioViewer/PortfolioViewer.WASM/Models/HoldingIdentifierMappingModel.cs
+++ PortfolioViewer/PortfolioViewer.WASM/Models/HoldingIdentifierMappingModel.cs
@@ -1,5 +1,3 @@
-using GhostfolioSidekick.Model.Activities;
-
 namespace GhostfolioSidekick.PortfolioViewer.WASM.Models
 {
 	public class HoldingIdentifierMappingModel
@@ -9,35 +7,5 @@ namespace GhostfolioSidekick.PortfolioViewer.WASM.Models
 		public int HoldingId { get; set; }
 		public List<PartialIdentifierDisplayModel> PartialIdentifiers { get; set; } = [];
 		public List<DataProviderMappingModel> DataProviderMappings { get; set; } = [];
-	}
-
-	public class PartialIdentifierDisplayModel
-	{
-		public required string Identifier { get; set; }
-		public List<AssetClass>? AllowedAssetClasses { get; set; }
-		public List<AssetSubClass>? AllowedAssetSubClasses { get; set; }
-		public List<string> MatchedDataProviders { get; set; } = [];
-		public bool HasUnresolvedMapping { get; set; }
-		public string? Comment { get; set; }
-	}
-
-	public class DataProviderMappingModel
-	{
-		public required string DataSource { get; set; }
-		public required string Symbol { get; set; }
-		public required string Name { get; set; }
-		public string? ISIN { get; set; }
-		public AssetClass AssetClass { get; set; }
-		public AssetSubClass? AssetSubClass { get; set; }
-		public required string Currency { get; set; }
-		public List<string> Identifiers { get; set; } = [];
-		public List<string> MatchedPartialIdentifiers { get; set; } = [];
-	}
-
-	public class IdentifierMatchingHistoryModel
-	{
-		public required string PartialIdentifier { get; set; }
-		public required string DataSource { get; set; }
-		public required string MatchedSymbol { get; set; }
 	}
 }
---
+++ PortfolioViewer/PortfolioViewer.WASM/Models/PartialIdentifierDisplayModel.cs
@@ -0,0 +1,14 @@
+using GhostfolioSidekick.Model.Activities;
+
+namespace GhostfolioSidekick.PortfolioViewer.WASM.Models
+{
+	public class PartialIdentifierDisplayModel
+	{
+		public required string Identifier { get; set; }
+		public List<AssetClass>? AllowedAssetClasses { get; set; }
+		public List<AssetSubClass>? AllowedAssetSubClasses { get; set; }
+		public List<string> MatchedDataProviders { get; set; } = [];
+		public bool HasUnresolvedMapping { get; set; }
+		public string? Comment { get; set; }
+	}
+}
