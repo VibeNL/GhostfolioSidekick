4c16cd594fb7097c6de13681d66627ef59deff6e
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-11-13 15:33:04 +0100

    1

--- PortfolioViewer/PortfolioViewer.WASM.UITests/WasmTestHost.cs
+++ PortfolioViewer/PortfolioViewer.WASM.UITests/WasmTestHost.cs
@@ -1,21 +1,45 @@
 using System;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
 
 namespace PortfolioViewer.WASM.UITests
 {
     public class WasmTestHost : IDisposable
     {
-        private readonly WebApplicationFactory<GhostfolioSidekick.PortfolioViewer.WASM.Program> _factory;
+        private readonly WebApplicationFactory<Startup> _factory;
         public string BaseUrl => _factory.Server.BaseAddress?.ToString() ?? "http://localhost";
+        private bool _disposed;
 
         public WasmTestHost()
         {
-            _factory = new WebApplicationFactory<GhostfolioSidekick.PortfolioViewer.WASM.Program>();
+            _factory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    var webHostBuilder = builder as IWebHostBuilder;
+                    if (webHostBuilder != null)
+                    {
+                        webHostBuilder.UseContentRoot("../../PortfolioViewer/PortfolioViewer.WASM/wwwroot");
+                        webHostBuilder.UseEnvironment("Development");
+                    }
+                });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _factory.Dispose();
+                }
+                _disposed = true;
+            }
         }
 
         public void Dispose()
         {
-            _factory.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
