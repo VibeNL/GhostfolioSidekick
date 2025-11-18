b218e475ddf561c85264b0fb5d34a2c3cdbbdae7
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-11-18 09:07:48 +0100

    1

--- GhostfolioSidekick/MarketDataMaintainer/MarketDataGathererTask.cs
+++ GhostfolioSidekick/MarketDataMaintainer/MarketDataGathererTask.cs
@@ -1,5 +1,6 @@
 ﻿using GhostfolioSidekick.Database;
 using GhostfolioSidekick.ExternalDataProvider;
+using GhostfolioSidekick.Model;
 using GhostfolioSidekick.Model.Activities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -104,6 +105,32 @@ namespace GhostfolioSidekick.MarketDataMaintainer
 				foreach (var marketData in list)
 				{
 					var existingRecord = symbol.MarketData.SingleOrDefault(x => x.Date == marketData.Date);
+					decimal closeAmount = marketData.Close.Amount;
+					if (closeAmount == 0)
+					{
+						// Find previous and next non-zero points by date
+						var previous = symbol.MarketData
+							.Where(x => x.Date < marketData.Date && x.Close.Amount != 0)
+							.OrderByDescending(x => x.Date)
+							.FirstOrDefault();
+						var next = symbol.MarketData
+							.Where(x => x.Date > marketData.Date && x.Close.Amount != 0)
+							.OrderBy(x => x.Date)
+							.FirstOrDefault();
+						if (previous != null && next != null)
+						{
+							var a = previous.Date.ToDateTime(TimeOnly.MinValue);
+							var b = marketData.Date.ToDateTime(TimeOnly.MinValue);
+							var c = next.Date.ToDateTime(TimeOnly.MinValue);
+							var total = (c - a).TotalDays;
+							var prevWeight = (c - b).TotalDays / total;
+							var nextWeight = (b - a).TotalDays / total;
+							var weighted = previous.Close.Amount * (decimal)prevWeight + next.Close.Amount * (decimal)nextWeight;
+							marketData.Close = new Money(marketData.Close.Currency, weighted);
+							marketData.IsGenerated = true;
+						}
+					}
+
 					if (existingRecord != null && Math.Abs(existingRecord.Close.Amount - marketData.Close.Amount) < 0.00001m)
 					{
 						continue;
