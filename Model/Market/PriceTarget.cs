230ac32727c8764321fe731023b4c84e8e0210ac
Vincent <30174292+VibeNL@users.noreply.github.com>
2025-12-16 16:45:45 +0100

    outline

---
+++ Model/Market/PriceTarget.cs
@@ -0,0 +1,21 @@
+﻿namespace GhostfolioSidekick.Model.Market
+{
+	public class PriceTarget
+	{
+		public int Id { get; set; }
+
+		public Money HighestTargetPrice { get; set; } = default!;
+
+		public Money AverageTargetPrice { get; set; } = default!;
+
+		public Money LowestTargetPrice { get; set; } = default!;
+
+		public AnalystRating Rating { get; set; }
+
+		public int NumberOfBuys { get; set; }
+
+		public int NumberOfHolds { get; set; }
+
+		public int NumberOfSells { get; set; }
+	}
+}
