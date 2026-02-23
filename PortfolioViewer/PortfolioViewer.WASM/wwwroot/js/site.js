016789e2982ce4655617aa75b1eff7eabb0762d7
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-02-23 20:58:16 +0100

    1

--- PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.js
+++ PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.js
@@ -1,27 +1,32 @@
-"use strict";
-function scrollToBottom(id) {
-	const element = document.getElementById(id);
-	if (element) {
-		element.scrollTop = element.scrollHeight;
-	}
-}
-
-// Unregister service workers and force reload to ensure new version loads
-globalThis.forceBlazorReload = async function () {
-	if ('serviceWorker' in navigator) {
-		const registrations = await navigator.serviceWorker.getRegistrations();
-		for (let registration of registrations) {
-			await registration.unregister();
-		}
-	}
-
-	// Clear all browser caches
-	if (globalThis.caches) {
-		const cacheNames = await caches.keys();
-		for (const name of cacheNames) {
-			await caches.delete(name);
-		}
-	}
-
-	globalThis.location.reload();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+function scrollToBottom(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.scrollTop = element.scrollHeight;
+    }
+}
+globalThis.forceBlazorReload = () => __awaiter(void 0, void 0, void 0, function* () {
+    if ('serviceWorker' in navigator) {
+        const registrations = yield navigator.serviceWorker.getRegistrations();
+        for (const registration of registrations) {
+            yield registration.unregister();
+        }
+    }
+    // Clear all browser caches
+    if ('caches' in globalThis) {
+        const cacheNames = yield caches.keys();
+        for (const name of cacheNames) {
+            yield caches.delete(name);
+        }
+    }
+    globalThis.location.reload();
+});
+export {};
--- PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.ts
+++ PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.ts
@@ -4,3 +4,25 @@ function scrollToBottom(id: string): void {
         element.scrollTop = element.scrollHeight;
     }
 }
+
+// Explicitly define and assign the function to globalThis
+const forceBlazorReload = async (): Promise<void> => {
+    if ('serviceWorker' in navigator) {
+        const registrations = await navigator.serviceWorker.getRegistrations();
+        for (const registration of registrations) {
+            await registration.unregister();
+        }
+    }
+
+    // Clear all browser caches
+    if ('caches' in globalThis) {
+        const cacheNames = await caches.keys();
+        for (const name of cacheNames) {
+            await caches.delete(name);
+        }
+    }
+
+    globalThis.location.reload();
+};
+
+(globalThis as any).forceBlazorReload = forceBlazorReload;
