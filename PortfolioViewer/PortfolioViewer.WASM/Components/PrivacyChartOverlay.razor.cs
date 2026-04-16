630751bc1d32116f252a46cf9785791ee22c4794
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-04-16 15:06:54 +0200

    format

--- PortfolioViewer/PortfolioViewer.WASM/Components/PrivacyChartOverlay.razor.cs
+++ PortfolioViewer/PortfolioViewer.WASM/Components/PrivacyChartOverlay.razor.cs
@@ -2,23 +2,23 @@ using Microsoft.AspNetCore.Components;
 
 namespace GhostfolioSidekick.PortfolioViewer.WASM.Components
 {
-    public partial class PrivacyChartOverlay : ComponentBase, IDisposable
-    {
-        [Parameter] public RenderFragment? ChildContent { get; set; }
+	public partial class PrivacyChartOverlay : ComponentBase, IDisposable
+	{
+		[Parameter] public RenderFragment? ChildContent { get; set; }
 
-        protected override void OnInitialized()
-        {
-            PrivacyModeService.OnChange += OnPrivacyModeChanged;
-        }
+		protected override void OnInitialized()
+		{
+			PrivacyModeService.OnChange += OnPrivacyModeChanged;
+		}
 
-        private void OnPrivacyModeChanged()
-        {
-            InvokeAsync(StateHasChanged);
-        }
+		private void OnPrivacyModeChanged()
+		{
+			InvokeAsync(StateHasChanged);
+		}
 
-        public void Dispose()
-        {
-            PrivacyModeService.OnChange -= OnPrivacyModeChanged;
-        }
-    }
+		public void Dispose()
+		{
+			PrivacyModeService.OnChange -= OnPrivacyModeChanged;
+		}
+	}
 }
--- PortfolioViewer/PortfolioViewer.WASM/Components/PrivacyValue.razor.cs
+++ PortfolioViewer/PortfolioViewer.WASM/Components/PrivacyValue.razor.cs
@@ -2,23 +2,23 @@ using Microsoft.AspNetCore.Components;
 
 namespace GhostfolioSidekick.PortfolioViewer.WASM.Components
 {
-    public partial class PrivacyValue : ComponentBase, IDisposable
-    {
-        [Parameter, EditorRequired] public string Value { get; set; } = string.Empty;
+	public partial class PrivacyValue : ComponentBase, IDisposable
+	{
+		[Parameter, EditorRequired] public string Value { get; set; } = string.Empty;
 
-        protected override void OnInitialized()
-        {
-            PrivacyModeService.OnChange += OnPrivacyModeChanged;
-        }
+		protected override void OnInitialized()
+		{
+			PrivacyModeService.OnChange += OnPrivacyModeChanged;
+		}
 
-        private void OnPrivacyModeChanged()
-        {
-            InvokeAsync(StateHasChanged);
-        }
+		private void OnPrivacyModeChanged()
+		{
+			InvokeAsync(StateHasChanged);
+		}
 
-        public void Dispose()
-        {
-            PrivacyModeService.OnChange -= OnPrivacyModeChanged;
-        }
-    }
+		public void Dispose()
+		{
+			PrivacyModeService.OnChange -= OnPrivacyModeChanged;
+		}
+	}
 }
