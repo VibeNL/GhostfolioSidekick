36dfa4dc19fc7b343b7834b8e8fc6b32ac47f748
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-04-18 16:56:59 +0200

    added tests

--- Model.UnitTests/Activities/PartialActivityTests.cs
+++ Model.UnitTests/Activities/PartialActivityTests.cs
@@ -36,5 +36,100 @@ namespace GhostfolioSidekick.Model.UnitTests.Activities
             var activity = PartialActivity.CreateCashDeposit(Currency.USD, DateTime.UtcNow, amount, new Money(Currency.USD, 1), "tx1");
             Assert.Equal(amount, activity.Amount);
         }
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateCashWithdrawal_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateCashWithdrawal(Currency.USD, DateTime.UtcNow, amount, new Money(Currency.USD, 1), "tx2"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateBuy_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateBuy(Currency.USD, DateTime.UtcNow, new List<PartialSymbolIdentifier?>(), amount, new Money(Currency.USD, 1), new Money(Currency.USD, 1), "tx3"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateSell_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateSell(Currency.USD, DateTime.UtcNow, new List<PartialSymbolIdentifier?>(), amount, new Money(Currency.USD, 1), new Money(Currency.USD, 1), "tx4"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateGiftFiat_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateGift(Currency.USD, DateTime.UtcNow, amount, new Money(Currency.USD, 1), "tx5"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateGiftAsset_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateGift(DateTime.UtcNow, new List<PartialSymbolIdentifier?>(), amount, "tx6"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateSend_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateSend(DateTime.UtcNow, new List<PartialSymbolIdentifier?>(), amount, "tx7"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateReceive_ShouldThrow_OnNegativeAmount(decimal amount)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateReceive(DateTime.UtcNow, new List<PartialSymbolIdentifier?>(), amount, "tx8"));
+            Assert.Contains("Amount cannot be negative", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateCurrencyConvert_ShouldThrow_OnNegativeSourceOrTarget(decimal negative)
+        {
+            var date = DateTime.UtcNow;
+            var total = new Money(Currency.USD, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateCurrencyConvert(date, new Money(Currency.USD, negative), new Money(Currency.EUR, 1), total, "tx9").GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateCurrencyConvert(date, new Money(Currency.USD, 1), new Money(Currency.EUR, negative), total, "tx10").GetEnumerator().MoveNext());
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void CreateAssetConvert_ShouldThrow_OnNegativeSourceOrTarget(decimal negative)
+        {
+            var date = DateTime.UtcNow;
+            var syms = new List<PartialSymbolIdentifier?>();
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateAssetConvert(date, syms, negative, syms, 1, "tx11").GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PartialActivity.CreateAssetConvert(date, syms, 1, syms, negative, "tx12").GetEnumerator().MoveNext());
+        }
     }
 }
