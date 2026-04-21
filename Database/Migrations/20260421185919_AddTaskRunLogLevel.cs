efa84d07d233fe221d19d5057e7ec5f36f9ad83f
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-04-21 20:59:30 +0200

    add migration

---
+++ Database/Migrations/20260421185919_AddTaskRunLogLevel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GhostfolioSidekick.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskRunLogLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Severity",
+                table: "TaskRunLogs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Severity",
+                table: "TaskRunLogs");
+        }
+    }
+}
