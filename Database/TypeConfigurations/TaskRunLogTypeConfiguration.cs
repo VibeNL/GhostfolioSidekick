755fda94f3e6acfc4d91efbbda6ceb470e1e3b49
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-04-25 09:34:19 +0200

    Fixing task display

--- Database/TypeConfigurations/TaskRunLogTypeConfiguration.cs
+++ Database/TypeConfigurations/TaskRunLogTypeConfiguration.cs
@@ -1,17 +1,25 @@
 using GhostfolioSidekick.Model.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Globalization;
 
 namespace GhostfolioSidekick.Database.TypeConfigurations
 {
 	internal class TaskRunLogTypeConfiguration : IEntityTypeConfiguration<TaskRunLog>
 	{
+		private static readonly ValueConverter<DateTimeOffset, string> DateTimeOffsetConverter =
+			new(
+				v => v.ToString("O", CultureInfo.InvariantCulture),
+				v => DateTimeOffset.Parse(v, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind));
+
 		public void Configure(EntityTypeBuilder<TaskRunLog> builder)
 		{
 			builder.ToTable("TaskRunLogs");
 			builder.HasKey(l => l.Id);
 			builder.Property(l => l.Message).IsRequired();
 			builder.Property(l => l.TaskRunType).IsRequired();
+			builder.Property(l => l.Timestamp).HasConversion(DateTimeOffsetConverter);
 			builder.HasOne(l => l.TaskRun)
 				.WithMany(t => t.Logs)
 				.HasForeignKey(l => l.TaskRunType)
--- Database/TypeConfigurations/TaskRunTypeConfiguration.cs
+++ Database/TypeConfigurations/TaskRunTypeConfiguration.cs
@@ -1,15 +1,32 @@
-﻿using GhostfolioSidekick.Model.Tasks;
+using GhostfolioSidekick.Model.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Globalization;
 
 namespace GhostfolioSidekick.Database.TypeConfigurations
 {
 	internal class TaskRunTypeConfiguration : IEntityTypeConfiguration<TaskRun>
 	{
+		private static readonly ValueConverter<DateTimeOffset, string> DateTimeOffsetConverter =
+			new(
+				v => v.ToString("O", CultureInfo.InvariantCulture),
+				v => DateTimeOffset.Parse(v, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind));
+
+		private static readonly ValueConverter<DateTimeOffset?, string?> NullableDateTimeOffsetConverter =
+			new(
+				v => v.HasValue ? v.Value.ToString("O", CultureInfo.InvariantCulture) : null,
+				v => v != null ? DateTimeOffset.Parse(v, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind) : null);
+
 		public void Configure(EntityTypeBuilder<TaskRun> builder)
 		{
 			builder.ToTable("TaskRuns");
 			builder.HasKey(a => a.Type);
+
+			builder.Property(a => a.LastUpdate).HasConversion(DateTimeOffsetConverter);
+			builder.Property(a => a.NextSchedule).HasConversion(DateTimeOffsetConverter);
+			builder.Property(a => a.StartTime).HasConversion(NullableDateTimeOffsetConverter);
+			builder.Property(a => a.EndTime).HasConversion(NullableDateTimeOffsetConverter);
 		}
 	}
 }
