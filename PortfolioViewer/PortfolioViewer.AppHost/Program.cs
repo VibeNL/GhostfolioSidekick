4eed3b9556e929bdb11efa3cdd8a0bcd68059ac0
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-07-01 14:19:19 +0200

    1

--- PortfolioViewer/PortfolioViewer.AppHost/Program.cs
+++ PortfolioViewer/PortfolioViewer.AppHost/Program.cs
@@ -3,6 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var blazorapp = builder.AddBlazorWasmProject<Projects.PortfolioViewer_WASM>("blazorapp");
 var apiService = builder.AddProject<Projects.PortfolioViewer_ApiService>("apiservice");
 builder.AddProject<Projects.PortfolioViewer_WASM>("blazorServer")
+						.WithExternalHttpEndpoints()
 						.WithBlazorClientApp(blazorapp)
 						.WithReference(apiService);
 
