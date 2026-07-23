cb94ef32f4285c7581b1b1528f866780a739176d
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-07-23 15:48:17 +0200

    Package upgrades

--- PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.js
+++ PortfolioViewer/PortfolioViewer.WASM/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function scrollToBottom(id) {
     const element = document.getElementById(id);
     if (element) {
@@ -6,21 +15,21 @@ function scrollToBottom(id) {
     }
 }
 // Explicitly define and assign the function to globalThis
-const forceBlazorReload = async () => {
+const forceBlazorReload = () => __awaiter(void 0, void 0, void 0, function* () {
     if ('serviceWorker' in navigator) {
-        const registrations = await navigator.serviceWorker.getRegistrations();
+        const registrations = yield navigator.serviceWorker.getRegistrations();
         for (const registration of registrations) {
-            await registration.unregister();
+            yield registration.unregister();
         }
     }
     // Clear all browser caches
     if ('caches' in globalThis) {
-        const cacheNames = await caches.keys();
+        const cacheNames = yield caches.keys();
         for (const name of cacheNames) {
-            await caches.delete(name);
+            yield caches.delete(name);
         }
     }
     globalThis.location.reload();
-};
+});
 globalThis.forceBlazorReload = forceBlazorReload;
 //# sourceMappingURL=site.js.map
