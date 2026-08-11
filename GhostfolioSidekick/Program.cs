76d21239e515b1e3b932941bed57ab5e4651bd06
Vincent <30174292+VibeNL@users.noreply.github.com>
2026-08-11 16:58:56 +0200

    Remove unused files

--- GhostfolioSidekick/Program.cs
+++ GhostfolioSidekick/Program.cs
@@ -148,7 +148,6 @@ namespace GhostfolioSidekick
 								]);
 							_ = services.AddSingleton<IStockPriceRepository[]>(sp => [sp.GetRequiredService<YahooRepository>(), sp.GetRequiredService<CoinGeckoRepository>(), sp.GetRequiredService<ManualSymbolRepository>()]);
 							_ = services.AddSingleton<IStockSplitRepository[]>(sp => [sp.GetRequiredService<YahooRepository>()]);
-							_ = services.AddSingleton<IPriceTargetRepository, PriceTargetRepository>();
 							_ = services.AddSingleton<IGhostfolioSync, GhostfolioSync>();
 							_ = services.AddSingleton<IRestCall>(sp =>
 							{
